5f03b0e30a11f61561b7836c9b86a85c488a6eba
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-02-17 20:35:23 +0300

    [New] integrate material ui pro theme

--- frontend/src/components/Routes.tsx
+++ frontend/src/components/Routes.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 
-const Root = () => <div>DDW Analyst UI</div>;
+const Root = () => (
+  <Grid className="col-lg-4">
+    <div>DDW Analyst UI</div>
+  </Grid>
+);
 
 export const Routes = () => (
   <BrowserRouter>
