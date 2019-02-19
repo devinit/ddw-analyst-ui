ec51faff357db492598971a43a0692883a8e01be
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-02-19 10:37:34 +0300

    [New] SocialLine component

---
+++ frontend/src/components/SocialLine/SocialLine.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Button } from 'react-bootstrap';
+import { ConditionalRender } from '../ConditionalRender';
+
+interface SocialLineProps {
+  google?: boolean;
+  facebook?: boolean;
+  twitter?: boolean;
+  onGoogle?: () => void;
+  onFacebook?: () => void;
+  onTwitter?: () => void;
+}
+
+export const SocialLine: React.SFC<SocialLineProps> = props => {
+  return (
+    <div className="social-line">
+      <ConditionalRender render={ !!props.google }>
+        <Button variant="link" className="btn-white" onClick={ props.onGoogle }>
+          <i className="fa fa-google"/>
+        </Button>
+      </ConditionalRender>
+      <ConditionalRender render={ !!props.facebook }>
+        <Button variant="link" className="btn-white" onClick={ props.onFacebook }>
+          <i className="fa fa-facebook"/>
+        </Button>
+      </ConditionalRender>
+      <ConditionalRender render={ !!props.twitter }>
+        <Button variant="link" className="btn-white" onClick={ props.onTwitter }>
+          <i className="fa fa-twitter"/>
+        </Button>
+      </ConditionalRender>
+    </div>
+  );
+};
