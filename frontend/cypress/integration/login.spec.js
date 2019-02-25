b04049893c7b5ad58ebde69594502ae888cd0583
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-02-26 02:56:14 +0300

    [New] add cypress for e2e testing + login tests

---
+++ frontend/cypress/integration/login.spec.js
@@ -0,0 +1,39 @@
+
+describe('The Login Page', () => {
+  it('should be redirected to when a user is not logged in', () => {
+    cy.visit('/');
+    cy.url().should('includes', '/login');
+  });
+
+  it('renders a loading indicator', () => {
+    cy.visit('/login');
+    cy.contains('Loading...');
+  });
+
+  describe('login form', () => {
+    it('requires an email and password', () => {
+      cy.visit('/login');
+      cy.wait(200);
+      cy.get('button[type=submit]').click();
+      cy.get('.invalid-feedback').contains('Username is required');
+      cy.get('.invalid-feedback').contains('Password is required');
+    });
+
+    it('displays error message when login fails', () => {
+      cy.visit('/login');
+      cy.get('input[name=username]').type('test@email.com');
+      cy.get('input[name=password]').type('fail_password');
+      cy.get('button[type=submit]').click();
+      cy.get('.alert').contains('Invalid username/password');
+    });
+
+    it('successfully performs a login action', () => {
+      cy.visit('/');
+      cy.wait(200);
+      cy.get('input[name=username]').type('admin');
+      cy.get('input[name=password]').type('3dw1NKsl?.');
+      cy.get('button[type=submit]').click();
+      cy.url().should('not.include', '/login');
+    });
+  });
+});
