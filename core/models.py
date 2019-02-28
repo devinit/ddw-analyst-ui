45738da0f949a6756ad40641a13488a75ff7d3d2
Alex Miller <alex.k.miller@gmail.com>
2019-02-28 11:47:41 -0500

    Explicit core module import

--- core/models.py
+++ core/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 # TODO: Discuss with team if there is a better way to managing migration and inspectdb at the same time
-from models_template import *
+from core.models_template import *
