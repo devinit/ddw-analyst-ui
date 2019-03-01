39d35b65f3e1ba503d9a0c3b32a4e05d191764e5
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-01 15:23:56 +0300

    [New] SidebarLink unit tests

---
+++ frontend/src/components/SidebarLink/__tests__/SidebarLink.spec.tsx
@@ -0,0 +1,80 @@
+/**
+ * @jest-environment jsdom
+ */
+import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import { SidebarLink } from '../SidebarLink';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+test('renders the correct link with the default props', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink to="link"/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
+
+test('renders root links correctly', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink to="link" root/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
+
+test('renders with a caret when specified', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink to="link" caret/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
+
+test('renders root links with a label when specified', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink root to="link" textNormal="Label"/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
+
+test('renders non-root links with both labels', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink to="link" textNormal="Label" textMini="L"/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
+
+test('renders with an icon when specified', () => {
+  const renderer = TestRenderer
+    .create(
+      <Router history={ createBrowserHistory() }>
+        <SidebarLink to="link" icon="home"/>
+      </Router>
+    )
+    .toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
