91c70f3f2f432a751210d596e13d776591822c3a
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-04 15:42:31 +0300

    [Fix] import classnames properly

--- frontend/src/components/SidebarItem/SidebarItem.tsx
+++ frontend/src/components/SidebarItem/SidebarItem.tsx
@@ -1,6 +1,6 @@
+import classNames from 'classnames';
 import * as React from 'react';
 import { SidebarLink, SidebarLinkProps } from '../SidebarLink';
-import classNames = require('classnames');
 
 export interface SidebarItemProps {
   collapseId?: string;
