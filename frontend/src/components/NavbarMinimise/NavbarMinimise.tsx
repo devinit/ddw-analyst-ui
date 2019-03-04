8e1b66a8669173bef1b6dd9e5217a9df2d3c6c61
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-04 19:00:24 +0300

    [Update] handle NavbarMinimise toggle

--- frontend/src/components/NavbarMinimise/NavbarMinimise.tsx
+++ frontend/src/components/NavbarMinimise/NavbarMinimise.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 
-export const NavbarMinimise: React.SFC = () => {
-  return (
-    <div className="navbar-minimize">
-      <button id="minimizeSidebar" className="btn btn-just-icon btn-white btn-fab btn-round">
-        <i className="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
-        <i className="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
-      </button>
-    </div>
-  );
-};
+export class NavbarMinimise extends React.Component {
+  render() {
+    return (
+      <div className="navbar-minimize">
+        <button className="btn btn-just-icon btn-white btn-fab btn-round" onClick={ this.onClick }>
+          <i className="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
+          <i className="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
+        </button>
+      </div>
+    );
+  }
+
+  private onClick() {
+    if (document.body.className.indexOf('sidebar-mini') > -1) {
+      document.body.classList.remove('sidebar-mini');
+    } else {
+      document.body.classList.add('sidebar-mini');
+    }
+  }
+}
