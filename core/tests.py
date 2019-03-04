cd0e77aa90e21bf5b44d997aa9bd82ad42a0bc01
Alex Miller <alex.k.miller@gmail.com>
2019-03-04 15:41:38 -0500

    Testing that adding a tag increments the count of the audits by 1

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from core.models import Tag, AuditLogEntry
 
-# Create your tests here.
-class SampleTestCase(TestCase):
-  def test_sample(self):
-    self.assertEqual('sample'.upper(), 'SAMPLE')
+
+class TestAuditLog(TestCase):
+    """Test case class for testing functionality of audit log"""
+    def test_increment_audit_log(self):
+        initial_audit_count = AuditLogEntry.objects.count()
+        new_tag = Tag(name="test_tag")
+        new_tag.save()
+        final_audit_count = AuditLogEntry.objects.count()
+        self.assertEqual(initial_audit_count + 1, final_audit_count)
