1091f7805404a34c6ee30494508ba63ebed5ac8f
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-04 15:46:36 +0300

    [Update] use babel, mock style modules for jest, remove snapshot update

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: 'ts-loader'
+        exclude: /node_modules/,
+        use: 'babel-loader'
       },
       {
           test: /\.(c|sc)ss$/,
