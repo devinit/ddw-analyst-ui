596807f3a443db8f54f6b32c7f254f49622f37d3
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-10 11:42:59 +0300

    [Fix] Login page unit tests

--- frontend/src/pages/Login/__tests__/Login.spec.tsx
+++ frontend/src/pages/Login/__tests__/Login.spec.tsx
@@ -1,19 +1,27 @@
 /**
  * @jest-environment jsdom
  */
+import 'jest-dom/extend-expect';
 import * as React from 'react';
 import { MemoryRouter, withRouter } from 'react-router-dom';
 import * as TestRenderer from 'react-test-renderer';
 import { cleanup, render, waitForElement } from 'react-testing-library';
+import { LoginActions } from '..';
 import { Login } from '../Login';
-import 'jest-dom/extend-expect';
 
+let actions: LoginActions;
 afterEach(cleanup);
+beforeEach(() => {
+  actions = {
+    setUser: jest.fn(),
+    setToken: jest.fn()
+  };
+});
 
 test('renders the loading indicator while loading', () => {
   const RouterContainer = withRouter(Login);
   const renderer = TestRenderer
-    .create(<MemoryRouter><RouterContainer/></MemoryRouter>)
+    .create(<MemoryRouter><RouterContainer actions={ actions }/></MemoryRouter>)
     .toJSON();
 
   expect(renderer).toMatchSnapshot();
@@ -21,7 +29,7 @@ test('renders the loading indicator while loading', () => {
 
 test('renders the login form when loading is false', async () => {
   const RouterContainer = withRouter(Login);
-  const { getByTestId } = render(<MemoryRouter><RouterContainer/></MemoryRouter>);
+  const { getByTestId } = render(<MemoryRouter><RouterContainer actions={ actions }/></MemoryRouter>);
   const Form = await waitForElement(() => getByTestId('login-form'));
 
   expect(Form).toMatchSnapshot();
