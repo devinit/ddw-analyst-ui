341c01c3ed895e0a330288ba949a5fd22b6747fc
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-13 14:11:27 +0300

    [New] SourceMetadata unit tests

---
+++ frontend/src/components/SourceMetadata/__tests__/SourceMetadata.spec.tsx
@@ -0,0 +1,17 @@
+import { List, Map } from 'immutable';
+import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import { ColumnList, SourceMap, UpdateHistoryList } from '../../../reducers/sources';
+import { SourceMetadata } from '../SourceMetadata';
+
+const columns: ColumnList = List([ Map({ source_name: 'Column 1', description: 'Column One' }) ]) as ColumnList;
+const updateHistory: UpdateHistoryList = List([
+  Map({ released_on: new Date('August 19, 2018 23:15:30').toISOString(), release_description: 'My Release' })
+]) as UpdateHistoryList;
+const source: SourceMap = Map({ columns, update_history: updateHistory, source_acronym: 'msc' }) as SourceMap;
+
+test('renders correctly with default props', () => {
+  const renderer = TestRenderer.create(<SourceMetadata source={ source }/>).toJSON();
+
+  expect(renderer).toMatchSnapshot();
+});
