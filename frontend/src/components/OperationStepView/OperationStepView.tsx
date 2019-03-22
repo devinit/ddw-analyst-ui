545fb9f58130798ae7711715d47f4bf8e17530d4
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-03-22 14:43:47 +0300

    [Update] show tooltip with description on hover

--- frontend/src/components/OperationStepView/OperationStepView.tsx
+++ frontend/src/components/OperationStepView/OperationStepView.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { Badge } from 'react-bootstrap';
+import { Popup } from 'semantic-ui-react';
 import { OperationStepMap } from '../../types/query-builder';
+import styled from 'styled-components';
 
 interface OperationStepProps {
   step: OperationStepMap;
 }
+
+const StyledPopContent = styled.div`font-size: 0.8rem;`;
+const StyledPopup = styled(Popup)`
+  &.ui.popup {
+    border: none;
+  }
+`;
 
 export class OperationStepView extends React.Component<OperationStepProps> {
   render() {
@@ -19,9 +28,21 @@ export class OperationStepView extends React.Component<OperationStepProps> {
         </div>
         <div>
           { step.get('name') }
-          <i className="material-icons float-right">info</i>
+          { this.renderStepInfo(step) }
         </div>
       </React.Fragment>
     );
+  }
+
+  private renderStepInfo(step: OperationStepMap) {
+    if (step.get('description')) {
+      return (
+        <StyledPopup
+          trigger={ <i className="material-icons float-right" data-testid="step-info-trigger">info</i> }
+          content={ <StyledPopContent>{ step.get('description') }</StyledPopContent> }
+          basic
+        />
+      );
+    }
   }
 }
