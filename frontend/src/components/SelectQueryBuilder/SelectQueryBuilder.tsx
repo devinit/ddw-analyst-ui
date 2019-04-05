a2d4bf987ccd0677f26f0c2e88edceccaf34fb3d
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-04-05 13:12:16 +0300

    [Update] Select/Deselect All

--- frontend/src/components/SelectQueryBuilder/SelectQueryBuilder.tsx
+++ frontend/src/components/SelectQueryBuilder/SelectQueryBuilder.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ColumnList, SourceMap } from '../../types/sources';
 import { Dropdown, DropdownItemProps, DropdownProps } from 'semantic-ui-react';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 interface SelectQueryBuilderProps {
   source: SourceMap;
@@ -14,19 +14,31 @@ export class SelectQueryBuilder extends React.Component<SelectQueryBuilderProps>
     const columns = this.props.source.get('columns') as ColumnList;
 
     return (
-      <Form.Group>
-        <Form.Label className="bmd-label-floating">Columns</Form.Label>
-        <Dropdown
-          placeholder="Select Columns"
-          fluid
-          multiple
-          search
-          selection
-          options={ this.getSelectOptionsFromColumns(columns) }
-          defaultValue={ this.props.columns }
-          onChange={ this.onChange }
-        />
-      </Form.Group>
+      <React.Fragment>
+        <Form.Group>
+          <Form.Label className="bmd-label-floating">Columns</Form.Label>
+          <Dropdown
+            placeholder="Select Columns"
+            fluid
+            multiple
+            search
+            selection
+            options={ this.getSelectOptionsFromColumns(columns) }
+            value={ this.props.columns }
+            onChange={ this.onChange }
+          />
+        </Form.Group>
+        <Form.Group>
+          <Button variant="danger" size="sm" onClick={ this.onSelectAll }>
+            <i className="material-icons mr-1">check_box</i>
+            Select All
+          </Button>
+          <Button variant="danger" size="sm" onClick={ this.onDeselectAll }>
+            <i className="material-icons mr-1">check_box_outline_blank</i>
+            Deselect All
+          </Button>
+        </Form.Group>
+      </React.Fragment>
     );
   }
 
@@ -45,6 +57,20 @@ export class SelectQueryBuilder extends React.Component<SelectQueryBuilderProps>
   private onChange = (_event: React.SyntheticEvent<HTMLElement, Event>, data: DropdownProps) => {
     if (this.props.onUpdateColumns) {
       this.props.onUpdateColumns(JSON.stringify({ columns: data.value as string[] }));
+    }
+  }
+
+  private onSelectAll = () => {
+    if (this.props.onUpdateColumns) {
+      const columns = this.props.source.get('columns') as ColumnList;
+      const columnNames = columns.map(column => column.get('name')).toJS();
+      this.props.onUpdateColumns(JSON.stringify({ columns: columnNames as string[] }));
+    }
+  }
+
+  private onDeselectAll = () => {
+    if (this.props.onUpdateColumns) {
+      this.props.onUpdateColumns(JSON.stringify({ columns: [] }));
     }
   }
 }
