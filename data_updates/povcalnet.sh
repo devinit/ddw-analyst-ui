e916a5b2c649fd8f7a8bac81dd5736ca208a8965
Alex Miller <alex.k.miller@gmail.com>
2019-04-08 13:25:33 -0400

    povcal dist

--- data_updates/povcalnet.sh
+++ data_updates/povcalnet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd /root/ddw-analyst-ui
-source env/bin/activate && python3 data_updates/Python/povcal_agg.py && python3 data_updates/Python/povcal_smy.py && python3 data_updates/Python/povcal_p20.py
+source env/bin/activate
+python3 data_updates/Python/povcal_agg.py  # "repo"."PovCalNetAgg"
+python3 data_updates/Python/povcal_smy.py  # "repo"."PovCalNetSmy"
+python3 data_updates/Python/povcal_p20.py  # "repo"."PovCalNetP20"
+Rscript data_updates/R/povcal_dist.R  # "repo"."PovCalNetDist"
