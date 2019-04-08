a49b9ad62003aafa8ad737f909235f8b54630c5a
Alex Miller <alex.k.miller@gmail.com>
2019-04-08 15:23:13 -0400

    Manual data sh

---
+++ data_updates/Python/manual_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import glob
+import os
+
+
+def main():
+    engine = create_engine('postgresql://postgres@/analyst_ui')
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    glob_path = os.path.join(dir_path, "..", "manual", "CSV", "*.csv")
+    csv_paths = glob.glob(glob_path)
+    abs_csv_paths = [os.path.abspath(csv) for csv in csv_paths]
+    for abs_csv_path in abs_csv_paths:
+        basename = os.path.basename(abs_csv_path)
+        table_name = os.path.splitext(basename)[0]
+        if table_name not in ["meta", "meta_columns"]:
+            csv_dat = pd.read_csv(abs_csv_path)
+            csv_dat.to_sql(name=table_name, con=engine, schema="repo", index=False, if_exists="replace")
+
+
+if __name__ == '__main__':
+    main()
---
+++ data_updates/manual_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /root/ddw-analyst-ui
+source env/bin/activate
+python3 data_updates/Python/manual_data.py
