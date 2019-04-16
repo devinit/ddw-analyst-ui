4c69c53c64cddabaeeffd18fbac66360a5d6557b
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-04-16 14:49:58 +0300

    [Update] add error handling on QueryBuilder

--- frontend/src/components/QueryBuilderHandler/QueryBuilderHandler.tsx
+++ frontend/src/components/QueryBuilderHandler/QueryBuilderHandler.tsx
@@ -7,6 +7,7 @@ import FilterQueryBuilder from '../FilterQueryBuilder';
 import { JoinQueryBuilder } from '../JoinQueryBuilder';
 import { SelectQueryBuilder } from '../SelectQueryBuilder';
 import { TransformQueryBuilder } from '../TransformQueryBuilder';
+import { Alert } from 'react-bootstrap';
 
 interface QueryBuilderHandlerProps {
   source: SourceMap;
@@ -16,6 +17,19 @@ interface QueryBuilderHandlerProps {
 
 export class QueryBuilderHandler extends React.Component<QueryBuilderHandlerProps> {
   render() {
+    try {
+      return this.renderQueryBuilder();
+    } catch (error) {
+      if (error.message && error.message.indexOf('JSON')) {
+        return <Alert variant="warning">Invalid JSON in step options. Delete and recreate the step.</Alert>;
+      }
+      console.log(error); //tslint:disable-line
+
+      return <Alert variant="warning">Failed to process step options. Delete and recreate the step.</Alert>;
+    }
+  }
+
+  renderQueryBuilder() {
     const { onUpdateOptions, step, source } = this.props;
     const query = step.get('query_func');
     const options = step.get('query_kwargs') as string;
