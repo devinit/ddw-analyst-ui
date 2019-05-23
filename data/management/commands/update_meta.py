41dc05024a27e13b32441ceb6cdceddeb1f6d2c7
Alex Miller <alex.k.miller@gmail.com>
2019-05-23 13:13:19 -0400

    Update meta command and test

---
+++ data/management/commands/update_meta.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from core.models import Source, UpdateHistory
+
+
+class Command(BaseCommand):
+    help = 'Updates metadata when an automated dataset gets updated'
+
+    def add_arguments(self, parser):
+        parser.add_argument('active_mirror_name', nargs='?', type=str)
+        parser.add_argument('release_description', nargs='?', type=str, default="Automated update.")
+
+    def handle(self, *args, **options):
+        source = Source.objects.get(active_mirror_name=options["active_mirror_name"])
+        update_history = UpdateHistory(
+            source=source,
+            release_description=options["release_description"]
+        )
+        update_history.save()
