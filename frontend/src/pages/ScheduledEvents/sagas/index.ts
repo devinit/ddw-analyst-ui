07c8643c29abeac0e722ab4707ddbfc623caf707
mariamiah <mariah.kakande@gmail.com>
2020-05-22 10:30:21 +0300

    setup redux-saga to fetch scheduledEvents api

---
+++ frontend/src/pages/ScheduledEvents/sagas/index.ts
@@ -0,0 +1,8 @@
+// function* fetchScheduledEventsData({ payload }) {
+//   console.log(payload);
+// }
+
+export function* scheduledEventsSagas() {
+  yield null;
+  // yield takeLatest(SET_ACTIVE_SCHEDULED_EVENT, fetchScheduledEventsData);
+}
