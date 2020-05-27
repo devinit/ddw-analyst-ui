970210d210d1110ebf708e03719804370668d58f
David Ebukali <debukali@gmail.com>
2020-05-27 16:27:47 +0300

    Create and update scheduled run instances after script has completed execution

--- data/management/commands/run_schedules.py
+++ data/management/commands/run_schedules.py
@@ -1,7 +1,13 @@
+import json
 from django.core.management.base import BaseCommand
 from datetime import date, datetime
 from core.models import ScheduledEvent, ScheduledEventRunInstance
-from core.tasks import execute_script
+from data_updates.utils import ScriptExecutor
+from django.http import StreamingHttpResponse
+from rest_framework import status
+from django.http import HttpResponse
+from django.utils.timezone import make_aware
+
 
 class Command(BaseCommand):
     help = 'Executes scheduled events at their appointed time'
@@ -74,27 +80,65 @@ class Command(BaseCommand):
 
     def check_if_schedule_is_due(self, schedule, run_instances):
         if run_instances.count() <= 0:
-            print(schedule.id)
-            print('never been run')
+            # If the count of run instances is zero, return true because the schedule has never been run
             return True
         elif run_instances.count() > 0:
             for run_instance in run_instances:
                 if run_instance.ended_at is None:
-                    print('sth runing')
+                    # If ended_at is null return false because there's a run instance in progress
                     return False
             if self.manage_repeated_schedules(schedule, run_instances):
                 return True
             else:
                 return False
 
+    def execute_script(self, script_name):
+        post_status = status.HTTP_200_OK
+        executor = ScriptExecutor(script_name)
+        stream = executor.stream()
+        response_data = {}
+        response_data['result'] = 'success'
+        response_data['message'] = 'Script update success'
+
+        for item in stream:
+            pass
+        # Check if the last item in generator is an integer
+        # The integer is a return code showing 0 for success or anything else for a file execute error
+        if item > 0 or item < 0:
+            post_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response_data['result'] = 'error'
+            response_data['message'] = 'Failed to execute the script update'
+            response_data['return_code'] = item
+
+        return response_data
+
     def handle(self, *args, **kwargs):
-        print('running')
-        execute_script.delay('fts.sh')
-        update_response = ''
         schedules = ScheduledEvent.objects.filter(enabled=True)
         for schedule in schedules:
             run_instances = ScheduledEventRunInstance.objects.filter(scheduled_event=schedule.id)
             if self.check_if_schedule_is_due(schedule, run_instances):
-                print('running')
-                execute_script.delay(schedule.script_name)
-                self.stdout.write('Update Status Code for ' + schedule.script_name + ' is ' + str(update_response))
+
+                #Create run instance
+                runInstance = ScheduledEventRunInstance(
+                    scheduled_event = schedule,
+                    start_at = make_aware(datetime.now()),
+                    status = 'r'
+                )
+                runInstance.save()
+
+                #Run the script
+                update_response = self.execute_script(schedule.script_name)
+
+                #Check if script run was a success/fail and update run instance
+                if update_response['return_code'] > 0 or update_response['return_code'] < 0:
+                    ScheduledEventRunInstance.objects.filter(pk=runInstance.id).update(
+                        ended_at=make_aware(datetime.now()),
+                        status = 'e'
+                    )
+                    self.stdout.write('Update failed for ' + schedule.script_name)
+                else:
+                    ScheduledEventRunInstance.objects.filter(pk=runInstance.id).update(
+                        ended_at=make_aware(datetime.now()),
+                        status = 'c'
+                    )
+                    self.stdout.write('Update successful for ' + schedule.script_name)
