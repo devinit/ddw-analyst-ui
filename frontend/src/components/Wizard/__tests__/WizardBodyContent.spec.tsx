0121431a43fe24b31f748d814831b6cc5e4db72d
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-04 18:01:18 +0300

    Add lazy loading to WizardBodyContent

--- frontend/src/components/Wizard/__tests__/WizardBodyContent.spec.tsx
+++ frontend/src/components/Wizard/__tests__/WizardBodyContent.spec.tsx
@@ -4,6 +4,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import * as TestRenderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { WizardBodyContent } from '../WizardBodyContent';
 
 describe('WizardBodyContent', () => {
@@ -13,7 +14,17 @@ describe('WizardBodyContent', () => {
     expect(renderer).toMatchSnapshot();
   });
 
-  test('renders with children', () => {
+  test('renders with children when active', () => {
+    const renderer = TestRenderer.create(
+      <WizardBodyContent active>
+        <div>My Child</div>
+      </WizardBodyContent>,
+    ).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+
+  test('renders no children when inactive by default', () => {
     const renderer = TestRenderer.create(
       <WizardBodyContent>
         <div>My Child</div>
@@ -21,5 +32,32 @@ describe('WizardBodyContent', () => {
     ).toJSON();
 
     expect(renderer).toMatchSnapshot();
+  });
+
+  test('always renders with children when viewed', () => {
+    const { getByTestId, rerender } = render(
+      <WizardBodyContent>
+        <div>My Child</div>
+      </WizardBodyContent>,
+    );
+    const tabPane = getByTestId('wizard-tab-pane');
+
+    expect(tabPane.innerHTML).toBe('');
+
+    rerender(
+      <WizardBodyContent active>
+        <div>My Child</div>
+      </WizardBodyContent>,
+    );
+
+    expect(tabPane.innerHTML).toBe('<div>My Child</div>');
+
+    rerender(
+      <WizardBodyContent>
+        <div>My Child</div>
+      </WizardBodyContent>,
+    );
+
+    expect(tabPane.innerHTML).toBe('<div>My Child</div>');
   });
 });
