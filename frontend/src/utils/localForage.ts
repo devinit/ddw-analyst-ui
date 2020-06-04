06feed53adfc622cf3c4c16f221bf007678a2d7e
mariamiah <mariah.kakande@gmail.com>
2020-06-04 09:09:49 +0300

    fetch run history

--- frontend/src/utils/localForage.ts
+++ frontend/src/utils/localForage.ts
@@ -3,6 +3,7 @@ import * as localForage from 'localforage';
 export const localForageKeys = {
   API_KEY: 'API_KEY',
   USER: 'USER',
+  EVENT_ID: 'EVENT_ID',
 };
 
 export const clearStorage = () => localForage.clear();
