cbf5a713a7c158bc1451959b1fa1c28b3dddcc9b
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-23 11:39:46 +0300

    Show design pages only on debug mode

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    return render(request, 'frontend/index.html')
+    return render(request, 'frontend/index.html', { 'DEBUG': settings.DEBUG })
 
 def with_id(request, pk):
     return index(request)
 
 def scheduled_events(request):
-    return render(request, 'frontend/designs/scheduled_events.html')
+    if settings.DEBUG:
+        return render(request, 'frontend/designs/scheduled_events.html')
+
+    raise Http404()
 
 def update_data_source(request):
-    return render(request, 'frontend/designs/update_data_source.html')
+    if settings.DEBUG:
+        return render(request, 'frontend/designs/update_data_source.html')
+
+    raise Http404()
