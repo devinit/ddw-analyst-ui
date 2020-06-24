f50942d6892ad03fe3b738765c994fda4be5d9df
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-06-24 12:27:18 +0300

    Update js file code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BundleTracker = require('webpack-bundle-tracker');
@@ -5,36 +6,36 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: [ '@babel/polyfill', './frontend/src/index.ts' ],
+  entry: ['@babel/polyfill', './frontend/src/index.ts'],
   output: {
     path: path.resolve(__dirname, 'frontend/static/frontend/'),
     filename: 'js/bundle.js',
     chunkFilename: 'js/[name].bundle.js',
-    publicPath: '/static/frontend/'
+    publicPath: '/static/frontend/',
   },
   resolve: {
-    extensions: [ '.ts', '.tsx', '.js' ]
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
-          test: /\.(c|sc)ss$/,
-          use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-      }
-    ]
+        test: /\.(c|sc)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   devtool: 'eval-source-map',
   watch: true,
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/bundle.css'
+      filename: 'css/bundle.css',
     }),
-    new BundleTracker({filename: './webpack-stats.json'}),
-    new LiveReloadPlugin()
-]
+    new BundleTracker({ filename: './webpack-stats.json' }),
+    new LiveReloadPlugin(),
+  ],
 };
