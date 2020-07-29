eeaa625be3c93489877d999af689e46dbc4cb811
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-07-29 18:00:42 +0300

    View logs on run history table

--- frontend/src/components/ScheduledEventsRunHistoryTable/ScheduledEventsRunHistoryTable.tsx
+++ frontend/src/components/ScheduledEventsRunHistoryTable/ScheduledEventsRunHistoryTable.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { ScheduledEventRunHistory } from '../../types/scheduledEvents';
+import { BasicModal } from '../BasicModal';
 import { ScheduledEventsRunHistoryTableRow } from '../ScheduledEventsRunHistoryTableRow';
 
 interface ScheduledEventRunHistoryTableProps {
@@ -9,10 +10,17 @@ interface ScheduledEventRunHistoryTableProps {
 export const ScheduledEventsRunHistoryTable: FunctionComponent<ScheduledEventRunHistoryTableProps> = (
   props,
 ) => {
+  const [info, setInfo] = useState('');
+  const onViewLogs = (logs: string): void => setInfo(logs);
+  const onModalHide = () => setInfo('');
   const renderRows = (): ReactNode =>
     props.data
       ? props.data.map((history: ScheduledEventRunHistory, index: number) => (
-          <ScheduledEventsRunHistoryTableRow key={index} history={history} />
+          <ScheduledEventsRunHistoryTableRow
+            key={index}
+            history={history}
+            onViewLogs={onViewLogs}
+          />
         ))
       : null;
 
@@ -23,9 +31,13 @@ export const ScheduledEventsRunHistoryTable: FunctionComponent<ScheduledEventRun
           <th>Status</th>
           <th>Started</th>
           <th>Ended</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>{renderRows()}</tbody>
+      <BasicModal show={!!info} onHide={onModalHide}>
+        <p>{info}</p>
+      </BasicModal>
     </Table>
   );
 };
--- frontend/src/components/ScheduledEventsRunHistoryTableRow/ScheduledEventsRunHistoryTableRow.tsx
+++ frontend/src/components/ScheduledEventsRunHistoryTableRow/ScheduledEventsRunHistoryTableRow.tsx
@@ -1,27 +1,43 @@
 import classNames from 'classnames';
 import moment from 'moment';
 import React, { FunctionComponent } from 'react';
+import { Button } from 'react-bootstrap';
 import { ScheduledEventRunHistory } from '../../types/scheduledEvents';
 import { convertStatus, getStatusClasses } from './utils';
 
 interface ScheduledEventsRunHistoryTableRowProps {
   history: ScheduledEventRunHistory;
+  onViewLogs?: (info: string) => void;
 }
 
-export const ScheduledEventsRunHistoryTableRow: FunctionComponent<ScheduledEventsRunHistoryTableRowProps> = (
-  props,
-) => {
-  const badgeClass = getStatusClasses(props.history.status);
+export const ScheduledEventsRunHistoryTableRow: FunctionComponent<ScheduledEventsRunHistoryTableRowProps> = ({
+  history,
+  onViewLogs,
+}) => {
+  const badgeClass = getStatusClasses(history.status);
+  const onClickInfo = () => {
+    if (onViewLogs && history.logs) {
+      onViewLogs(history.logs);
+    }
+  };
 
   return (
     <tr>
       <td>
-        <span className={classNames('badge', badgeClass)}>
-          {convertStatus(props.history.status)}
-        </span>
+        <span className={classNames('badge', badgeClass)}>{convertStatus(history.status)}</span>
       </td>
-      <td>{moment.utc(props.history.start_at).format('LLL')}</td>
-      <td>{props.history.ended_at ? moment.utc(props.history.ended_at).format('LLL') : 'None'}</td>
+      <td>{moment.utc(history.start_at).format('LLL')}</td>
+      <td>{history.ended_at ? moment.utc(history.ended_at).format('LLL') : 'None'}</td>
+      <td>
+        {history.logs && onViewLogs ? (
+          <Button size="sm" variant="link" className="btn-danger" onClick={onClickInfo}>
+            <i className="material-icons" data-testid="info-trigger">
+              info
+            </i>{' '}
+            <span>Logs</span>
+          </Button>
+        ) : null}
+      </td>
     </tr>
   );
 };
