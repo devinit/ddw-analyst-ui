764faa374f90ac1884a55efb3a920c1e3b3d4b4f
David Ebukali <debukali@gmail.com>
2020-08-03 11:05:52 +0300

    Create new module from operation functions

---
+++ core/query.py
@@ -0,0 +1,17 @@
+from core.pypika_utils import QueryBuilder
+from data.db_manager import fetch_data
+from core.models import Source
+
+def build_query(operation=None, steps=None, limit=None, offset=None, estimate_count=None):
+        """Build an SQL query"""
+        count_query = QueryBuilder(operation=operation, operation_steps=steps, source=Source).count_sql(estimate_count)
+        if limit is None:
+            return (count_query, QueryBuilder(operation=operation, operation_steps=steps, source=Source).get_sql_without_limit())
+        return (count_query, QueryBuilder(operation=operation, operation_steps=steps, source=Source).get_sql(limit, offset))
+
+def query_table(limit, offset, estimate_count, op_steps=None, operation=None):
+    """Build a query then execute it to return the matching data"""
+    if limit is None or int(limit) > 10000:
+        limit = 10000
+    queries = build_query(limit=limit, offset=offset, estimate_count=estimate_count, steps=op_steps, operation=operation)
+    return fetch_data(queries)
