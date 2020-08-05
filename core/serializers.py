3e3d32b4286d0c1a91f8e856a59b6a79258c36f6
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-05 20:14:04 +0300

    Update existing operations with aliases on save

--- core/serializers.py
+++ core/serializers.py
@@ -208,6 +208,7 @@ class OperationSerializer(serializers.ModelSerializer):
 
         instance.operation_query = query.build_query(operation=instance)
         instance.save()
+        self.update_operation_data_aliases(instance)
 
         return instance
 
@@ -218,16 +219,32 @@ class OperationSerializer(serializers.ModelSerializer):
             first_step = operation.get_operation_steps()[0]
             columns = SourceColumnMap.objects.filter(source=first_step.source, name__in=data_column_keys)
             for column in data_column_keys:
-                matching = columns.filter(name=column)
-                if len(matching):
-                    alias = OperationDataColumnAlias.objects.create(
-                        operation=operation, column_name=column, column_alias=matching[0].alias)
-                else:
-                    alias = OperationDataColumnAlias.objects.create(
-                        operation=operation, column_name=column, column_alias=column)
+                matching = columns.filter(name=column).first()
+                alias = self.create_operation_alias(operation, column, matching.alias if matching else column)
                 alias.save()
         except: # FIXME: handle specific errors
             pass
+
+    def update_operation_data_aliases(self, operation):
+        count, data = operation.query_table(1, 0, estimate_count=True)
+        try:
+            data_column_keys = data[0].keys()
+            first_step = operation.get_operation_steps()[0]
+            columns = SourceColumnMap.objects.filter(source=first_step.source, name__in=data_column_keys)
+            # delete obsolete aliases
+            OperationDataColumnAlias.objects.filter(operation=operation).exclude(column_name__in=data_column_keys).delete()
+            for column in data_column_keys:
+                existing_alias = OperationDataColumnAlias.objects.filter(operation=operation, column_name=column).first()
+                if not existing_alias:
+                    matching = columns.filter(name=column).first()
+                    alias = self.create_operation_alias(operation, column, matching.alias if matching else column)
+                    alias.save()
+        except: # FIXME: handle specific errors
+            pass
+
+    def create_operation_alias(self, operation, column_name, column_alias):
+        return OperationDataColumnAlias.objects.create(
+            operation=operation, column_name=column_name, column_alias=column_alias)
 
 
 class ThemeSerializer(serializers.ModelSerializer):
