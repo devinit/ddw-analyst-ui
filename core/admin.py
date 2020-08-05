2980eb0138e7dca9a9fb916a934c702887b84323
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-05 16:44:08 +0300

    Add OperationDataColumnAlias to admin management

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ from core.models import (
     AuditLogEntry,
     Operation,
     OperationStep,
+    OperationDataColumnAlias,
     Review,
     ScheduledEvent,
     ScheduledEventRunInstance,
@@ -25,6 +26,7 @@ admin.site.register(Theme)
 admin.site.register(Tag)
 admin.site.register(Operation)
 admin.site.register(OperationStep)
+admin.site.register(OperationDataColumnAlias)
 admin.site.register(Review)
 admin.site.register(Source)
 admin.site.register(SourceColumnMap)
