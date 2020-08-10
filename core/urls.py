259543fcc2559ec41b81f5097f87308f6260802d
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-10 16:14:06 +0300

    Add API endpoint for retrieving, updating & deleting aliases

--- core/urls.py
+++ core/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path('dataset/<int:pk>/', core_views.OperationDetail.as_view()),
     path('dataset/preview/', core_views.PreviewOperationData.as_view()),
     path('dataset/data/<int:pk>/', core_views.ViewData.as_view()),
+    path('dataset/alias/<int:pk>/', core_views.OperationColumnAlias.as_view()),
 ]
 
 handler500 = 'rest_framework.exceptions.server_error'
