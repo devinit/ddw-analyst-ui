c67ee0bfce5a278e6f0407a7c61a881c63cbcd71
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-11 20:03:42 +0300

    Fix source metadata view in modal

--- frontend/src/components/SourceDetailsTab/__tests__/SourceDetailsTab.spec.tsx
+++ frontend/src/components/SourceDetailsTab/__tests__/SourceDetailsTab.spec.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import 'jest-styled-components';
 import { List, Map } from 'immutable';
 import * as React from 'react';
 import { render } from '@testing-library/react';
