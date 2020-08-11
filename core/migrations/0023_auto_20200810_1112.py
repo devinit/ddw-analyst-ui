20e77bbaab9b95317656e9fb7d7fba3c060b1a39
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-11 10:30:16 +0300

    Update auto migration with error handling

--- core/migrations/0023_auto_20200810_1112.py
+++ core/migrations/0023_auto_20200810_1112.py
@@ -2,6 +2,8 @@
 
 from django.db import migrations
 
+from json.decoder import JSONDecodeError
+
 from core import query
 
 def generate_aliases(apps, schema_editor):
@@ -14,17 +16,20 @@ def generate_aliases(apps, schema_editor):
     operations = Operation.objects.all()
 
     for operation in operations:
-        operation.operationdatacolumnalias_set.all().delete()
-        count, data = query.query_table(operation, 1, 0, estimate_count=True)
-        data_column_keys = data[0].keys()
-        first_step = operation.operationstep_set.order_by('step_id')[0]
-        columns = SourceColumnMap.objects.filter(source=first_step.source, name__in=data_column_keys)
+        try:
+            operation.operationdatacolumnalias_set.all().delete()
+            count, data = query.query_table(operation, 1, 0, estimate_count=True)
+            data_column_keys = data[0].keys()
+            first_step = operation.operationstep_set.order_by('step_id')[0]
+            columns = SourceColumnMap.objects.filter(source=first_step.source, name__in=data_column_keys)
 
-        for column in data_column_keys:
-            column_object = columns.filter(name=column).first()
-            alias = OperationDataColumnAlias.objects.create(operation=operation, column_name=column)
-            alias.column_alias = column_object.alias if column_object else column
-            alias.save()
+            for column in data_column_keys:
+                column_object = columns.filter(name=column).first()
+                alias = OperationDataColumnAlias.objects.create(operation=operation, column_name=column)
+                alias.column_alias = column_object.alias if column_object else column
+                alias.save()
+        except JSONDecodeError:
+            pass
 
 
 class Migration(migrations.Migration):
