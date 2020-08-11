1919782490feffa27bda39a058b6262ca1b71d73
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-11 21:06:41 +0300

    Update code style on Home async loader

--- frontend/src/pages/Home/AsyncLoader.tsx
+++ frontend/src/pages/Home/AsyncLoader.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import React, { ReactElement } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
 const Home = React.lazy(() => import('./Home'));
-export const AsyncHome = (props: RouteComponentProps) => (
+export const AsyncHome = (props: RouteComponentProps): ReactElement => (
   <React.Suspense
     fallback={
       <Dimmer active={true} inverted>
