8c2f9fbd01d27a2ac3320beea5b041af036298b4
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-13 17:00:38 +0300

    Revert to basic aliases in query error event

--- core/migrations/0023_auto_20200810_1112.py
+++ core/migrations/0023_auto_20200810_1112.py
@@ -29,7 +29,14 @@ def generate_aliases(apps, schema_editor):
                 alias.column_alias = column_object.alias if column_object else column
                 alias.save()
         except:
-            pass
+            first_step = operation.operationstep_set.order_by('step_id')[0]
+            if first_step:
+                columns = SourceColumnMap.objects.filter(source=first_step.source, name__in=data_column_keys)
+
+                for column in columns:
+                    alias = OperationDataColumnAlias.objects.create(operation=operation, column_name=column.name)
+                    alias.column_alias = column.alias
+                    alias.save()
 
 
 class Migration(migrations.Migration):
