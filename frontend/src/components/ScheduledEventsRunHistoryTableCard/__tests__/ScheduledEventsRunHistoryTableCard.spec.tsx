533554c596f753d9b5c26949925ab64d94c9fcab
mariamiah <mariah.kakande@gmail.com>
2020-08-16 15:07:49 +0300

    test scheduled events run history table card

---
+++ frontend/src/components/ScheduledEventsRunHistoryTableCard/__tests__/ScheduledEventsRunHistoryTableCard.spec.tsx
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { cleanup, render } from '@testing-library/react';
+import 'jest-styled-components';
+import React from 'react';
+import { ScheduledEvent } from '../../../types/scheduledEvents';
+import { ScheduledEventsRunHistoryTableCard } from '../ScheduledEventsRunHistoryTableCard';
+
+describe('<ScheduledEventsRunHistoryTableCard', () => {
+  const events: ScheduledEvent[] = [
+    {
+      id: 1,
+      name: 'event1',
+      description: null,
+      enabled: true,
+      interval: 5,
+      interval_type: 'secs',
+      repeat: true,
+      start_date: '',
+    },
+  ];
+  afterEach(cleanup);
+  const { container } = render(<ScheduledEventsRunHistoryTableCard event={events[0]} />);
+  it('should match snapshot', () => {
+    expect(container).toMatchSnapshot();
+  });
+});
