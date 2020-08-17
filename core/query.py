cab227b731b3c4d9acea4db776f9196f742081aa
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-08-17 14:46:30 +0300

    Recalculate count without estimate when 0

--- core/query.py
+++ core/query.py
@@ -4,6 +4,8 @@ from data.db_manager import fetch_data
 def build_query(operation=None, steps=None, limit=None, offset=None, estimate_count=None):
     """Build an SQL query"""
     count_query = QueryBuilder(operation=operation, operation_steps=steps).count_sql(estimate_count)
+    if not count_query: # if count returns 0, remove estimate TODO: figure out why count would contradict actual results
+        count_query = QueryBuilder(operation=operation, operation_steps=steps).count_sql(False)
     if limit is None:
         return (count_query, QueryBuilder(operation=operation, operation_steps=steps).get_sql_without_limit())
     return (count_query, QueryBuilder(operation=operation, operation_steps=steps).get_sql(limit, offset))
