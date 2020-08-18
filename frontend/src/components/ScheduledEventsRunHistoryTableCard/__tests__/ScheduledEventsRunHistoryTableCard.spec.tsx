220a58b183bf083e3d6e88a167934f0055406fbd
mariamiah <mariah.kakande@gmail.com>
2020-08-18 08:34:44 +0300

    test fetch data per page request

--- frontend/src/components/ScheduledEventsRunHistoryTableCard/__tests__/ScheduledEventsRunHistoryTableCard.spec.tsx
+++ frontend/src/components/ScheduledEventsRunHistoryTableCard/__tests__/ScheduledEventsRunHistoryTableCard.spec.tsx
@@ -2,48 +2,68 @@
  * @jest-environment jsdom
  */
 import '@testing-library/jest-dom/extend-expect';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, waitFor, act } from '@testing-library/react';
 import 'jest-styled-components';
 import React from 'react';
-// import axios from 'axios';
-// import renderer, { act } from 'react-test-renderer';
 import { ScheduledEventsRunHistoryTableCard } from '../ScheduledEventsRunHistoryTableCard';
+import { AxiosResponse } from 'axios';
+import localforage from 'localforage';
+
+jest.mock('localforage');
+const data = {
+  results: [
+    {
+      scheduled_event: 1,
+      start_at: new Date('2020-01-01').toISOString(),
+      status: 'pending',
+    },
+  ],
+  count: 1,
+};
+
+const axiosResponse: AxiosResponse = {
+  data: data,
+  status: 200,
+  statusText: 'OK',
+  config: {},
+  headers: {},
+};
+
+jest.mock('axios', () => ({
+  __esModule: true,
+  get: jest.fn(() => Promise.resolve(axiosResponse)),
+  default: jest.fn(() => Promise.resolve(axiosResponse)),
+}));
 
 describe('<ScheduledEventsRunHistoryTableCard', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    await localforage.setItem('localForageKeys.API_KEY', '0695904053');
+  });
+  const event = {
+    id: 1,
+    name: 'event 1',
+    description: null,
+    enabled: true,
+    interval: 2,
+    interval_type: 'secs',
+    repeat: true,
+    start_date: new Date('2020-01-01').toISOString(),
+  };
   afterEach(cleanup);
-  const { container } = render(<ScheduledEventsRunHistoryTableCard />);
-  it('should match snapshot', () => {
-    expect(container).toMatchSnapshot();
+  afterEach(() => jest.clearAllMocks());
+
+  it('should match snapshot', async () => {
+    await act(async () => {
+      const { container } = render(<ScheduledEventsRunHistoryTableCard event={event} />);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  // it('should fetch data per page on page load', async () => {
-  //   const mProps = {
-  //     query: 'pippo',
-  //     onQueryChange: jest.fn(),
-  //   };
-  //   const FAKE_HITS = [{ objectID: 1, url: 'haha.com', title: 'haha' }];
-  //   const axiosGetSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: { hits: FAKE_HITS } });
-  //   let component;
-  //   await act(async () => {
-  //     component = renderer.create(<DisplayData {...mProps}></DisplayData>);
-  //   });
-  //   expect(axiosGetSpy).toBeCalledWith('http://hn.algolia.com/api/v1/search?query=pippo');
-  //   expect(component.toJSON()).toMatchSnapshot();
-  //   axiosGetSpy.mockRestore();
-  // });
+  it('should fetch data per page on page load', async () => {
+    await act(async () => {
+      const { getByText } = render(<ScheduledEventsRunHistoryTableCard event={event} />);
+      await waitFor(() => expect(getByText('Showing 1 to 1 of 1')));
+    });
+  });
 });
-
-// it('should not fetch data when query is empty string', async () => {
-//   const mProps = {
-//     query: '',
-//     onQueryChange: jest.fn()
-//   };
-//   const axiosGetSpy = jest.spyOn(axios, 'get');
-//   let component;
-//   await act(async () => {
-//     component = renderer.create(<DisplayData {...mProps}></DisplayData>);
-//   });
-//   expect(axiosGetSpy).not.toBeCalled();
-//   expect(component.toJSON()).toMatchSnapshot();
-//   axiosGetSpy.mockRestore();
-// });
--- frontend/src/components/ScheduledEventsTableRow/__tests__/ScheduledEventsTableRow.spec.tsx
+++ frontend/src/components/ScheduledEventsTableRow/__tests__/ScheduledEventsTableRow.spec.tsx
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { ScheduledEvent } from '../../../types/scheduledEvents';
@@ -9,6 +9,7 @@ import { ScheduledEventsTableRow } from '../ScheduledEventsTableRow';
 
 describe('<ScheduledEventsTableRow', () => {
   afterEach(cleanup);
+  const tablebody = document.createElement('tbody');
   const event: ScheduledEvent = {
     id: 1,
     name: 'event1',
@@ -19,9 +20,17 @@ describe('<ScheduledEventsTableRow', () => {
     interval_type: 'secs',
     repeat: true,
   };
-
   it('should take a snapshot', () => {
     const tree = renderer.create(<ScheduledEventsTableRow id={1} event={event} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('creates instance on button click', () => {
+    const { getByTestId } = render(<ScheduledEventsTableRow id={1} event={event} />, {
+      container: document.body.appendChild(tablebody),
+    });
+    const button = getByTestId('testruninstance');
+    fireEvent.click(button);
+    expect(button.innerHTML).toEqual('Creating instance...');
   });
 });
