d66a5fdaac389d9c742ebc9dfefce40fc2bc70c2
mariamiah <mariah.kakande@gmail.com>
2020-08-26 10:20:39 +0300

    change row colour on click

--- frontend/src/components/ScheduledEventsTableRow/ScheduledEventsTableRow.tsx
+++ frontend/src/components/ScheduledEventsTableRow/ScheduledEventsTableRow.tsx
@@ -53,7 +53,7 @@ export const ScheduledEventsTableRow: FunctionComponent<ScheduledEventsTableRowP
       <BasicModal show={modalShow} onHide={() => setModalShow(false)}>
         <p>{modalDescription}</p>
       </BasicModal>
-      <tr onClick={onRowClick} className={props.classNames}>
+      <tr onClick={onRowClick} className={props.classNames} data-testid="sche-events-table-row">
         <td className="text-center">{props.id}</td>
         <td>{props.event.name}</td>
         <td>{props.event.description}</td>
