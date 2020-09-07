8e593722ae23ff6a1f5dae382ee524e66f9fbdcd
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-09-07 17:46:56 +0300

    Use async celery task to count operation rows

---
+++ core/tasks.py
@@ -0,0 +1,14 @@
+from ddw_analyst_ui.celery import app
+
+from core.pypika_utils import QueryBuilder
+from data.db_manager import count_rows
+
+@app.task(bind=False)
+def count_operation_rows(operation):
+    count_query = QueryBuilder(operation=operation, operation_steps=None).count_sql(False)
+    count = count_rows(count_query)
+    operation.row_count = count
+    operation.count_rows = False
+    operation.save()
+
+    return 'Operation {} has {} rows'.format(operation.name, count)
