96239891d4e4fdc827f7c74a639670ff046a6429
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-09-08 17:36:50 +0300

    Pass operation ID to celery for count operation rows

--- core/tasks.py
+++ core/tasks.py
@@ -2,11 +2,17 @@ from ddw_analyst_ui.celery import app
 
 from core.pypika_utils import QueryBuilder
 from data.db_manager import count_rows
+from core.models import Operation
 
 @app.task(bind=False)
-def count_operation_rows(operation):
-    count_query = QueryBuilder(operation=operation, operation_steps=None).count_sql(False)
-    count = count_rows(count_query)
-    operation.row_count = count
-    operation.count_rows = False
-    operation.save()
+def count_operation_rows(id):
+    operation = Operation.objects.get(id=id)
+    if operation:
+        count_query = QueryBuilder(operation=operation, operation_steps=None).count_sql(False)
+        count = count_rows(count_query)
+        operation.row_count = count
+        operation.count_rows = False
+        operation.save()
+        return { "status": "success" }
+    else:
+        return { "status": "failed" }
--- frontend/models.py
+++ frontend/models.py
@@ -9,6 +9,6 @@ from core.tasks import count_operation_rows as count
 @receiver(post_save, sender=Operation, dispatch_uid="count_operation_rows")
 def count_operation_rows(sender, instance=None, created=False, **kwargs):
     if instance and instance.count_rows:
-        count.delay(instance)
+        count.delay(instance.id)
 
 request_finished.connect(count_operation_rows, dispatch_uid="count_operation_rows")
