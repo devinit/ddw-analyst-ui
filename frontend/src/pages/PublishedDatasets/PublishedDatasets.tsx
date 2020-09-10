2637c614648b190dfcfbe524f5cc1895ff40ab6b
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-09-10 11:40:03 +0300

    Add basic published datasets page

---
+++ frontend/src/pages/PublishedDatasets/PublishedDatasets.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { RouteComponentProps } from 'react-router-dom';
+import { OperationsTableCard } from '../../components/OperationsTableCard';
+
+type PageProps = RouteComponentProps;
+
+export const PublishedDatasets: FunctionComponent<PageProps> = () => {
+  return (
+    <Row>
+      <Col>
+        <OperationsTableCard limit={10} offset={0} />
+      </Col>
+    </Row>
+  );
+};
+
+export { PublishedDatasets as default };
