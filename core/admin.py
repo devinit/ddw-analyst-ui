8798b81a54e055499df825ba9032a028250297b4
David Ebukali <debukali@gmail.com>
2020-09-11 14:23:59 +0300

    Add email notifications model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from core.models import (
+    Alert,
     AuditLogEntry,
     Operation,
     OperationStep,
@@ -34,3 +35,4 @@ admin.site.register(UpdateHistory)
 admin.site.register(AuditLogEntry, AuditLogEntryAdmin)
 admin.site.register(ScheduledEvent)
 admin.site.register(ScheduledEventRunInstance)
+admin.site.register(Alert)
