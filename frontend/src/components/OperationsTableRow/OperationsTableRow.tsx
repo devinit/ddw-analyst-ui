e94fa5636792502ba64a2f3a1ed6a0fa513d8010
David Ebukali <debukali@gmail.com>
2020-09-24 10:32:06 +0300

    Test datasets with listview

--- frontend/src/components/OperationsTableRow/OperationsTableRow.tsx
+++ frontend/src/components/OperationsTableRow/OperationsTableRow.tsx
@@ -8,6 +8,8 @@ export interface OperationsTableRowProps {
   updatedOn: string;
   isDraft: boolean;
   classNames?: string;
+  description?: string;
+  user?: string;
 }
 
 export const OperationsTableRow: FunctionComponent<OperationsTableRowProps> = (props) => {
@@ -21,15 +23,15 @@ export const OperationsTableRow: FunctionComponent<OperationsTableRowProps> = (p
   return (
     <div className="dataset-row p-3 border-bottom">
       <div className="col-md-12">
-        <DatasetRowItem text="Hola" addClass="dataset-row-title h4">
-          <span className="badge badge-warning align-middle">Draft</span>
+        <DatasetRowItem text={props.name} addClass="dataset-row-title h4">
+          {props.isDraft ? <span className="badge badge-warning align-middle">Draft</span> : null}
         </DatasetRowItem>
-        <DatasetRowItem addClass="mb-2" text="Lets rock people we got this"></DatasetRowItem>
+        <DatasetRowItem addClass="mb-2" text={props.description}></DatasetRowItem>
 
         <DatasetRowItem addClass="dataset-row-actions mb-1">{renderActions()}</DatasetRowItem>
 
         <DatasetRowItem addClass="h6 dataset-row-footer" text="Updated 2 days ago by">
-          <span>Edwin P. Magezi</span> from <span className="text-uppercase text-danger">yolo</span>
+          <span>{props.user}</span> from <span className="text-uppercase text-danger">yolo</span>
         </DatasetRowItem>
       </div>
     </div>
