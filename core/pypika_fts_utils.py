bce80c54e83a5f9d5f2932bd2a3bebe9e1913683
Naphlin Peter Akena <wakibic@gmail.com>
2020-09-29 22:15:32 +0300

    Add schema support to drop/create table and support for "IF EXISTS" while dropping

--- core/pypika_fts_utils.py
+++ core/pypika_fts_utils.py
@@ -1,11 +1,15 @@
 from core import pypika_utils
+import re
 
 
 class TableQueryBuilder(pypika_utils.QueryBuilder):
 
     def __init__(self, initial_table_name, initial_schema_name, operation=None):
 
-        super().__init__(operation=operation)
+        if operation is not None:
+            super().__init__(operation=operation)
+
+        self.limit_regex = re.compile('LIMIT \d+', re.IGNORECASE)
         self.initial_table_name = initial_table_name
         self.initial_schema_name = initial_schema_name
 
@@ -24,14 +28,15 @@ class TableQueryBuilder(pypika_utils.QueryBuilder):
 
         return pypika_utils.Query.into(self.current_dataset).insert(*data).get_sql()
 
-    def create_table_from_query(self, table_name):
+    def create_table_from_query(self, table_name: str, schema_name: str) -> str:
         # CREATE TABLE table_name AS query
         select = self.current_query
+        table = pypika_utils.Table(table_name, schema_name)
         create_table = pypika_utils.Query.create_table(
-            table_name).as_select(select).get_sql()
+            table).as_select(select).get_sql()
         return create_table
 
-    def delete_table(self, table_name: str) -> str:
-        table = pypika_utils.Table(table_name)
-        sql = "DROP TABLE {table}".format(table=table.get_sql())
+    def delete_table(self, table_name: str, schema_name: str) -> str:
+        table = pypika_utils.Table(table_name, schema_name)
+        sql = "DROP TABLE IF EXISTS {table}".format(table=table.get_sql())
         return sql
