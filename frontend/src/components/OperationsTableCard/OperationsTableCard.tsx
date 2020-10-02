0994e757b5b25bdc8b3bc94bba47e0636c27bb0d
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-10-02 15:09:39 +0300

    Refactor dataset components

--- frontend/src/components/OperationsTableCard/OperationsTableCard.tsx
+++ frontend/src/components/OperationsTableCard/OperationsTableCard.tsx
@@ -1,27 +1,22 @@
 import { List } from 'immutable';
 import React, { FunctionComponent, useEffect, useState } from 'react';
-import { FormControl, OverlayTrigger, Popover } from 'react-bootstrap';
+import { Card, FormControl, OverlayTrigger, Popover, Form, Button } from 'react-bootstrap';
 import { connect, MapDispatchToProps } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { Dimmer, Loader } from 'semantic-ui-react';
+import styled from 'styled-components';
 import * as operationsActions from '../../actions/operations';
 import { OperationsState } from '../../reducers/operations';
 import { ReduxStore } from '../../store';
 import { LinksMap } from '../../types/api';
 import { FormControlElement } from '../../types/bootstrap';
-import { OperationMap, OperationStepMap } from '../../types/operations';
+import { OperationMap } from '../../types/operations';
 import { api } from '../../utils';
 import { BasicModal } from '../BasicModal';
-import { DatasetCardBody } from '../DatasetCardBody';
-import { DatasetCardFooter } from '../DatasetCardFooter';
-import { DatasetContent } from '../DatasetContent';
-import { OperationsTable } from '../OperationsTable';
 import { OperationsTableRow } from '../OperationsTableRow';
 import OperationsTableRowActions from '../OperationsTableRowActions';
 import { PaginationRow } from '../PaginationRow';
-import styled from 'styled-components';
-import { exportCsv } from './utils';
 
 interface ActionProps {
   actions: typeof operationsActions;
@@ -109,111 +104,68 @@ const OperationsTableCard: FunctionComponent<OperationsTableCardProps> = (props)
     }
   };
 
-  const viewData = (operation: OperationMap) => (
+  const onViewData = (operation: OperationMap) => (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) => {
     event.preventDefault();
-    const id = operation.get('id');
     props.actions.setOperation(operation);
-    props.history.push(`/queries/data/${id}`);
+    props.history.push(`/queries/data/${operation.get('id')}`);
   };
 
-  const viewSqlQuery = (operation: OperationMap) => (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  const onViewSQLQuery = (operation: OperationMap) => (
+    event: React.MouseEvent<HTMLButtonElement>,
   ) => {
     event.preventDefault();
     setInfo(operation.get('operation_query') as string);
   };
 
   const onEditOperation = (operation: OperationMap) => (
-    event: React.MouseEvent<HTMLAnchorElement | HTMLTableRowElement, MouseEvent>,
+    event: React.MouseEvent<HTMLAnchorElement>,
   ) => {
     event.preventDefault();
     props.history.push(`/queries/build/${operation.get('id') as number}/`);
   };
 
-  const onClickExportCsv = (operation: OperationMap) => (
-    event: React.MouseEvent<HTMLAnchorElement | HTMLTableRowElement, MouseEvent>,
-  ) => {
-    event.preventDefault();
-    exportCsv(operation.get('id') as number).then(function (response) {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${operation.get('name')}.csv`);
-      document.body.appendChild(link);
-      link.click();
-    });
-  };
-
-  const renderOperationsTable = (operations: List<OperationMap>, allowEdit = false) => {
-    const EditAction = ({ operation }: { operation: OperationMap }) => (
-      <a
-        className="btn btn-sm btn-dark"
-        href={`/queries/build/${operation.get('id') as number}/`}
-        onClick={onEditOperation(operation)}
-      >
-        Edit
-      </a>
-    );
-
+  const renderOperations = (operations: List<OperationMap>, allowEdit = false) => {
     if (operations && operations.count()) {
-      return (
-        <OperationsTable>
-          <DatasetCardBody removePadding={false}>
-            <FormControl
-              placeholder="Search ..."
-              className="w-25"
-              value={searchQuery}
-              onChange={onSearchChange}
-              onKeyDown={onSearch}
-              data-testid="sources-table-search"
-            />
-          </DatasetCardBody>
-          <BasicModal show={!!info} onHide={onModalHide}>
-            <StyledContent>{info}</StyledContent>
-          </BasicModal>
-          {operations.map((operation, index) => (
-            <OperationsTableRow
-              key={index}
-              count={index + 1}
-              name={operation.get('name') as string}
-              updatedOn={operation.get('updated_on') as string}
-              isDraft={operation.get('is_draft') as boolean}
-              description={operation.get('description') as string}
-              user={operation.get('user') as string}
-              updated_on={operation.get('updated_on') as string}
-              operation_steps={operation.get('operation_steps') as List<OperationStepMap>}
+      return operations.map((operation, index) => (
+        <OperationsTableRow key={index} operation={operation} showDraftBadge={showMyQueries}>
+          <OperationsTableRowActions>
+            {allowEdit ? (
+              <a
+                className="btn btn-sm btn-dark"
+                href={`/queries/build/${operation.get('id') as number}/`}
+                onClick={onEditOperation(operation)}
+              >
+                Edit
+              </a>
+            ) : null}
+            <OverlayTrigger
+              placement="top"
+              overlay={<Popover id="view">View Dataset Data</Popover>}
             >
-              <OperationsTableRowActions>
-                {allowEdit ? <EditAction operation={operation} /> : null}
-                <OverlayTrigger
-                  placement="top"
-                  overlay={<Popover id="view">View Operation Data</Popover>}
-                >
-                  <a
-                    className="btn btn-sm btn-dark"
-                    href={`/queries/data/${operation.get('id')}`}
-                    onClick={viewData(operation)}
-                  >
-                    View Data
-                  </a>
-                </OverlayTrigger>
-                <a className="btn btn-sm btn-dark" onClick={viewSqlQuery(operation)}>
-                  SQL Query
-                </a>
-                <a className="btn btn-sm btn-dark" onClick={onClickExportCsv(operation)}>
-                  Export to CSV
-                </a>
-              </OperationsTableRowActions>
-            </OperationsTableRow>
-          ))}
-          <DatasetCardFooter>{renderPagination()}</DatasetCardFooter>
-        </OperationsTable>
-      );
+              <a
+                className="btn btn-sm btn-dark"
+                href={`/queries/data/${operation.get('id')}/`}
+                onClick={onViewData(operation)}
+              >
+                View Data
+              </a>
+            </OverlayTrigger>
+            <Button variant="dark" size="sm" onClick={onViewSQLQuery(operation)}>
+              SQL Query
+            </Button>
+            <Form action={`${api.routes.EXPORT}${operation.get('id')}/`} method="POST">
+              <Button type="submit" variant="dark" size="sm">
+                Export to CSV
+              </Button>
+            </Form>
+          </OperationsTableRowActions>
+        </OperationsTableRow>
+      ));
     }
 
-    return <div className="mt-3">No results found</div>;
+    return <div className="px-4 pb-3">No datasets found</div>;
   };
 
   const onPageChange = (page: { selected: number }): void => {
@@ -253,12 +205,29 @@ const OperationsTableCard: FunctionComponent<OperationsTableCardProps> = (props)
   const loading = props.operations.get('loading') as boolean;
 
   return (
-    <React.Fragment>
+    <>
       <Dimmer active={loading} inverted>
         <Loader content="Loading" />
       </Dimmer>
-      <DatasetContent>{renderOperationsTable(operations, true)}</DatasetContent>
-    </React.Fragment>
+      <Card className="dataset-list">
+        <Card.Body>
+          <FormControl
+            placeholder="Search ..."
+            className="w-25"
+            value={searchQuery}
+            onChange={onSearchChange}
+            onKeyDown={onSearch}
+            data-testid="sources-table-search"
+          />
+        </Card.Body>
+        <Card.Body className="p-0">{renderOperations(operations, showMyQueries)}</Card.Body>
+        <Card.Footer className="d-block">{renderPagination()}</Card.Footer>
+
+        <BasicModal show={!!info} onHide={onModalHide}>
+          <StyledContent>{info}</StyledContent>
+        </BasicModal>
+      </Card>
+    </>
   );
 };
 
@@ -275,7 +244,7 @@ const mapStateToProps = (reduxStore: ReduxStore): ReduxState => {
   };
 };
 
-const connector = withRouter<ComponentProps, any>(
+const connector = withRouter<ComponentProps, any>( // eslint-disable-line @typescript-eslint/no-explicit-any
   connect(mapStateToProps, mapDispatchToProps)(OperationsTableCard),
 );
 
