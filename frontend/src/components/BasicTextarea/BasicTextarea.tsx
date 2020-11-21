73e59f698107f677c1700806a38334a5b6d57729
David Ebukali <debukali@gmail.com>
2020-11-21 14:40:45 +0300

    Add optional label prop to BasicTextarea

--- frontend/src/components/BasicTextarea/BasicTextarea.tsx
+++ frontend/src/components/BasicTextarea/BasicTextarea.tsx
@@ -3,18 +3,23 @@ import { Form } from 'react-bootstrap';
 import { FormControlElement } from '../../types/bootstrap';
 
 interface BasicTextareaProps {
+  label?: string;
   onChange: (options: string) => void;
-  alerts: { [key: string]: string } | undefined;
+  alerts?: { [key: string]: string } | undefined;
 }
 
-export const BasicTextarea: FunctionComponent<BasicTextareaProps> = ({ alerts, onChange }) => {
+export const BasicTextarea: FunctionComponent<BasicTextareaProps> = ({
+  alerts,
+  onChange,
+  label,
+}) => {
   const onTextareaChange = (event: ChangeEvent<FormControlElement>) => {
     onChange(event.currentTarget.value);
   };
 
   return (
     <Form.Group>
-      <Form.Label className="bmd-label-floating">Fill in your query below:</Form.Label>
+      <Form.Label className="bmd-label-floating">{label}</Form.Label>
       <Form.Control name="description" as="textarea" onChange={onTextareaChange} />
       <Form.Control.Feedback type="invalid" className="d-block invalid-feedback">
         {alerts && alerts.error}
