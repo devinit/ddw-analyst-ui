b3af4277318202f15c65667f02b122f53c5adb27
David Ebukali <debukali@gmail.com>
2020-11-24 08:21:15 +0300

    Edit text filter string

--- frontend/src/components/TextFilterQueryBuilder/TextFilterQueryBuilder.tsx
+++ frontend/src/components/TextFilterQueryBuilder/TextFilterQueryBuilder.tsx
@@ -1,10 +1,10 @@
 import { List, Set } from 'immutable';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { OperationStepMap } from '../../types/operations';
 import { ColumnList, SourceMap } from '../../types/sources';
 import { getStepSelectableColumns } from '../../utils';
 import { BasicTextarea } from '../BasicTextarea';
-import { parseTextFilterString } from './utils';
+import { getTextFilterString, parseTextFilterString } from './utils';
 
 interface TextFilterQueryBuilder {
   alerts: { [key: string]: string } | undefined;
@@ -22,19 +22,31 @@ export const TextFilterQueryBuilder: FunctionComponent<TextFilterQueryBuilder> =
   step,
   steps,
 }) => {
-  const textareaLabel = 'Fill in your query below:';
-  const columns = source.get('columns') as ColumnList;
-  const selectableColumns = getStepSelectableColumns(step, steps, columns) as Set<string>;
+  const [defaultValue, setDefaultValue] = useState('');
+  useEffect(() => {
+    const options = step.get('query_kwargs') as string;
+    const { filters } = options ? JSON.parse(options) : { filters: [] };
+    setDefaultValue(getTextFilterString('normal', filters ? filters : [], ''));
+  });
 
   const onTextareaChange = (options: string) => {
+    const columns = source.get('columns') as ColumnList;
+    const selectableColumns = getStepSelectableColumns(step, steps, columns) as Set<string>;
     const queryObject = parseTextFilterString(options, selectableColumns);
     console.log(`onTextareaChange: ${JSON.stringify(queryObject['filterJSON'])}`);
     onUpdateTextarea(JSON.stringify(queryObject));
   };
 
+  const textareaLabel = 'Fill in your query below:';
+
   return (
     <>
-      <BasicTextarea label={textareaLabel} onChange={onTextareaChange} alerts={alerts} />
+      <BasicTextarea
+        label={textareaLabel}
+        onChange={onTextareaChange}
+        alerts={alerts}
+        defaultValue={defaultValue}
+      />
     </>
   );
 };
