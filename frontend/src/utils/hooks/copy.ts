45a76d57f84f8364e8ac431eb97a7514ea0af969
David Ebukali <debukali@gmail.com>
2020-12-01 17:57:18 +0300

    Add function to copy text

---
+++ frontend/src/utils/hooks/copy.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import copy from 'copy-to-clipboard';
+
+export type CopyToClipboardType = (boolean | ((text: string) => void))[];
+
+export default function useCopyToClipboard(resetInterval: number): CopyToClipboardType {
+  const [isCopied, setCopied] = React.useState(false);
+
+  const handleCopy = React.useCallback((text: string) => {
+    if (typeof text === 'string' || typeof text === 'number') {
+      copy(text.toString());
+      setCopied(true);
+    } else {
+      setCopied(false);
+      console.error(`Cannot copy typeof ${typeof text} to clipboard, must be a string or number.`);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    let timeout: number;
+    if (isCopied && resetInterval) {
+      timeout = setTimeout(() => setCopied(false), resetInterval);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isCopied, resetInterval]);
+
+  return [isCopied, handleCopy];
+}
