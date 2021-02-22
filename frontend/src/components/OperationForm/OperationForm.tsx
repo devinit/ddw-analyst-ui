dc98bb04c7d8849addec65fe498339ebcfe446ed
David Ebukali <debukali@gmail.com>
2021-02-22 20:27:39 +0300

    Reinitialise query builder form when values change

--- frontend/src/components/OperationForm/OperationForm.tsx
+++ frontend/src/components/OperationForm/OperationForm.tsx
@@ -118,7 +118,13 @@ export const OperationForm: FunctionComponent<OperationFormProps> = (props) => {
   const values: Partial<Operation> = props.operation ? props.operation.toJS() : {};
 
   return (
-    <Formik validationSchema={schema} initialValues={values} onSubmit={onSuccess()} validateOnMount>
+    <Formik
+      enableReinitialize={true}
+      validationSchema={schema}
+      initialValues={values}
+      onSubmit={onSuccess()}
+      validateOnMount
+    >
       {({ errors, isSubmitting, isValid, setFieldValue }: FormikProps<Operation>) => (
         <Form className="form" noValidate data-testid="operation-form">
           <Alert variant="danger" hidden={!props.alert}>
