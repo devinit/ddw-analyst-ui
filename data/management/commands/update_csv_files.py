e80513d9fd117f7cf1700b6487ac7b0a3452d7f2
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-03 23:19:36 +0300

    Add operation logs on updating csv files

--- data/management/commands/update_csv_files.py
+++ data/management/commands/update_csv_files.py
@@ -1,12 +1,12 @@
-from django.core.management.base import BaseCommand, CommandError
-from github import Github
-import urllib.request
-from django.conf import settings
 import csv
 import os
+import urllib.request
 
-from core.models import SourceColumnMap
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from github import Github
 
+from core.models import OperationStep, SourceColumnMap
 
 COLUMNS_META_FILE_NAME = 'meta_columns.csv'
 GITHUB_REPO = 'devinit/ddw-data-update-configs'
@@ -37,9 +37,8 @@ class Command(BaseCommand):
                     urllib.request.urlretrieve(file_content.download_url, destination_path + file_content.path)
 
                     if COLUMNS_META_FILE_NAME in file_content.path:
+                        self.stdout.write("Checking columns...", ending='\n')
                         self.check_column_mapping(options["path"])
-
-                    break;
         except Exception as e:
             raise CommandError(e)
 
@@ -50,8 +49,7 @@ class Command(BaseCommand):
         for column in columns:
             source = column.source
             if not self.does_csv_have_matching_column(COLUMNS_META_PATH, source, column):
-                # TODO: look for usages of the affected column
-                pass
+                self.update_affected_columns(source, column)
 
     def does_csv_have_matching_column(self, csv_path, source, column):
         match_found = False
@@ -63,3 +61,34 @@ class Command(BaseCommand):
                     break
 
         return match_found
+
+    def update_affected_columns(self, source, column):
+        steps = OperationStep.objects.filter(source=source.id, query_kwargs__contains=column.name)
+        if steps.count():
+            self.stdout.write("{} steps found using the obsolete {} column in the {} table".format(
+                steps.count(), column.name, source.active_mirror_name), ending='\n')
+        for step in steps:
+            operation = step.operation
+            columns = operation.logs.get('columns', []) if operation.logs else []
+            if not column.name in columns:
+                columns.append(column.name)
+            steps = operation.logs.get('steps', []) if operation.logs else []
+            if not step.step_id in steps:
+                steps.append(step.step_id)
+            operation.logs = {
+                'type': 'warning',
+                'message': 'Obsolete Columns',
+                'columns': columns,
+                'steps': steps
+            }
+            operation.save()
+
+            step_columns = step.logs.get('columns', []) if step.logs else []
+            if not column.name in step_columns:
+                step_columns.append(column.name)
+            step.logs = {
+                'type': 'warning',
+                'message': 'Obsolete Columns',
+                'columns': step_columns
+            }
+            step.save()
