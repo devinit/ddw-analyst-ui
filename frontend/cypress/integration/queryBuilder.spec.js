c37f0a126a54f65c7d13f4bb6a375459b2eb1327
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-04 17:00:59 +0300

    Add cypress test for alias creation errors

--- frontend/cypress/integration/queryBuilder.spec.js
+++ frontend/cypress/integration/queryBuilder.spec.js
@@ -65,8 +65,17 @@ describe('The Query Builder', () => {
     cy.get('.invalid-feedback').should('not.be.visible');
   });
 
-  xit('that encountered an error or interruption during alias creation shows a warning', () => {
-    // TODO: add test
+  it('that encountered an error or interruption during alias creation shows a warning', () => {
+    // Mock datasets route
+    cy.fixture('datasets').then((datasets) => {
+      cy.intercept('api/datasets/mine/', datasets);
+    });
+
+    cy.visit('/');
+    cy.get('[data-testid="dataset-action"]').first().click({ force: true });
+    cy.get('[data-testid="qb-alert"] p').contains(
+      'There was interruption while creating column aliases for this dataset. Please save the dataset again',
+    );
   });
 
   xit('with an active data source has a button to add a step', () => {
