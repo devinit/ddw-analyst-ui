42deb9fe4c73eb4d6d42821894dc21a2fe29ed1f
ThatcherK <kaliithatmau@gmail.com>
2021-03-09 09:18:40 +0300

    Add tests for the moved copy button

--- frontend/cypress/integration/queryBuilder.spec.js
+++ frontend/cypress/integration/queryBuilder.spec.js
@@ -182,7 +182,19 @@ describe('The Query Builder', () => {
     // TODO: create test
   });
 
-  xit('makes a copy of a dataset', () => {
-    // TODO: create test
+  it('makes a copy of a dataset', () => {
+    cy.fixture('copyDataset').then((dataset) => {
+      cy.intercept('api/datasets/mine/', dataset);
+    });
+    cy.visit('/');
+    cy.get('[data-testid="dataset-action"]').first().click({ force: true });
+    cy.get('[data-testid="qb-duplicate-item"]').click();
+    cy.get('[data-testid="op-name-field"]').should('have.value', 'Copy of Test two');
+    cy.get('[data-testid="op-description-field"]').should('have.value', 'This is it');
+    cy.get('.form-check-input').should('be.checked');
+    cy.get('[data-testid="active-data-source"]')
+      .children()
+      .eq(1)
+      .contains('OECD Creditor Reporting System');
   });
 });
