f509cd52181ea8ec8f151bfa3a34c19d365f5461
ThatcherK <kaliithatmau@gmail.com>
2021-03-15 14:26:20 +0300

    Fix dataset test bug

--- frontend/cypress/integration/datasets.spec.js
+++ frontend/cypress/integration/datasets.spec.js
@@ -82,13 +82,13 @@ describe('The Datasets Pages', () => {
       cy.intercept('api/datasets/mine/', datasets);
     });
     cy.visit('/');
-    cy.get('[data-testid="dataset-duplicate"]').eq(1).click({ force: true });
-    cy.get('[data-testid="op-name-field"]').should('have.value', 'Copy of Test two');
+    cy.get('[data-testid="dataset-duplicate"]').eq(2).click({ force: true });
+    cy.get('[data-testid="op-name-field"]').should('have.value', 'Copy of Test Three');
     cy.get('[data-testid="op-description-field"]').should('have.value', 'This is it');
     cy.get('.form-check-input').should('be.checked');
     cy.get('[data-testid="active-data-source"]')
       .children()
       .eq(1)
-      .contains('OECD Creditor Reporting System');
+      .contains('International Offical Finance');
   });
 });
