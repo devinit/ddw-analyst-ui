34187e0e21b286a40e24e139d5cd5ed16f663025
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-18 17:55:21 +0300

    Add QueryBuilderContainer component

--- frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
+++ frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
+import { QueryBuilderContainer } from '../../components/QueryBuilderContainer';
+import { useSources } from '../../hooks';
 
 type RouterParams = {
   id?: string;
@@ -8,9 +10,23 @@ type RouterParams = {
 type QueryBuilderProps = RouteComponentProps<RouterParams>;
 
 const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = () => {
+  const sources = useSources({ limit: 200, offset: 0 });
+  console.log(sources.toJS());
+
   return (
     <Row>
-      <Col>Content Goes Here!</Col>
+      <Col>
+        <React.Fragment>
+          <Card>
+            <Card.Header className="card-header-text card-header-danger">
+              <Card.Title>Card Header Goes Here</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <QueryBuilderContainer />
+            </Card.Body>
+          </Card>
+        </React.Fragment>
+      </Col>
     </Row>
   );
 };
