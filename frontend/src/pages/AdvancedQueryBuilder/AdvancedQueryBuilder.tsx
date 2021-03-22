350ef894b91d20c5eea4af87621f5603e9985206
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-03-22 20:42:13 +0300

    Fetch operation for editing QB

--- frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
+++ frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
@@ -1,28 +1,37 @@
 import React, { FunctionComponent } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
+import { Dimmer, Loader } from 'semantic-ui-react';
 import { QueryBuilderContainer } from '../../components/QueryBuilderContainer';
-import { useSources } from '../../hooks';
+import { useOperation, useSources } from '../../hooks';
 
 type RouterParams = {
   id?: string;
 };
 type QueryBuilderProps = RouteComponentProps<RouterParams>;
 
-const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = () => {
+const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
+  const { id: operationID } = props.match.params;
+  const { loading, operation } = operationID
+    ? useOperation(parseInt(operationID))
+    : { loading: false, operation: undefined };
   const sources = useSources({ limit: 200, offset: 0 });
-  console.log(sources.toJS());
 
   return (
     <Row>
       <Col>
         <React.Fragment>
+          <Dimmer active={loading || !sources.count()} inverted>
+            <Loader content="Loading" />
+          </Dimmer>
           <Card>
             <Card.Header className="card-header-text card-header-danger">
               <Card.Title>Card Header Goes Here</Card.Title>
             </Card.Header>
             <Card.Body>
-              <QueryBuilderContainer />
+              {!loading && sources.count() ? (
+                <QueryBuilderContainer sources={sources.toJS()} operation={operation} />
+              ) : null}
             </Card.Body>
           </Card>
         </React.Fragment>
