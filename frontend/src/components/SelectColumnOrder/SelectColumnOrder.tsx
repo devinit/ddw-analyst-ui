dc652216cad8c9608dc3cae8074e4179194458d7
ThatcherK <kaliithatmau@gmail.com>
2021-04-12 17:33:37 +0300

    Add column sorting

---
+++ frontend/src/components/SelectColumnOrder/SelectColumnOrder.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent, useState } from 'react';
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
--- frontend/src/components/SelectQueryBuilder/SelectQueryBuilder.tsx
+++ frontend/src/components/SelectQueryBuilder/SelectQueryBuilder.tsx
@@ -1,3 +1,19 @@
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { List, Set } from 'immutable';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
@@ -32,6 +48,53 @@ const SelectQueryBuilder: FunctionComponent<SelectQueryBuilderProps> = (props) =
     );
   }, []);
 
+  const SelectColumn = (props) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+      id: props.id,
+    });
+    const style = {
+      transform: CSS.Translate.toString(transform),
+      transition,
+    };
+
+    return (
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        {props.id}
+      </div>
+    );
+  };
+  const SelectColumnOrder = () => {
+    const [items, setItems] = useState(props.columns ? props.columns.sort() : []);
+    const sensors = useSensors(
+      useSensor(PointerSensor),
+      useSensor(KeyboardSensor, {
+        coordinateGetter: sortableKeyboardCoordinates,
+      }),
+    );
+
+    const handleDragEnd = (event) => {
+      const { active, over } = event;
+      if (active.id !== over.id) {
+        setItems(() => {
+          const oldIndex = items.indexOf(active.id);
+          const newIndex = items.indexOf(over.id);
+
+          return arrayMove(items, oldIndex, newIndex);
+        });
+      }
+    };
+
+    return (
+      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {items.map((item) => (
+            <SelectColumn key={item} id={item} />
+          ))}
+        </SortableContext>
+      </DndContext>
+    );
+  };
+
   const onSelectAll = () => {
     if (props.onUpdateColumns) {
       const columns = props.source.get('columns') as ColumnList;
@@ -46,40 +109,53 @@ const SelectQueryBuilder: FunctionComponent<SelectQueryBuilderProps> = (props) =
     }
   };
 
+  const handleColumnOrderClick = () => {
+    setColumnOrderView(!columnOrderView);
+    // console.log(columnOrderView);
+  };
+
   return (
     <React.Fragment>
       <Form.Group>
         <Form.Label className="bmd-label-floating">Columns</Form.Label>
-        <Form.Row>
-          <Button
-            variant="danger"
-            size="sm"
-            onClick={onSelectAll}
-            hidden={!props.editable}
-            data-testid="qb-select-all-button"
-          >
-            <i className="material-icons mr-1">check_box</i>
-            Select All
-          </Button>
-          <Button
-            variant="danger"
-            size="sm"
-            onClick={onDeselectAll}
-            hidden={!props.editable}
-            data-testid="qb-select-none-button"
-          >
-            <i className="material-icons mr-1">check_box_outline_blank</i>
-            Deselect All
-          </Button>
-          <Button size="sm">{columnOrderView ? 'Select Columns' : 'Order Columns'}</Button>
-        </Form.Row>
-        <SelectColumnValidator step={props.step} steps={props.steps}>
-          <CheckboxGroup
-            options={selectableColumns.sort(sortObjectArrayByProperty('text').sort)}
-            selectedOptions={props.columns}
-            onUpdateOptions={props.onUpdateColumns}
-          />
-        </SelectColumnValidator>
+        <Button size="sm" onClick={handleColumnOrderClick}>
+          {columnOrderView ? 'Select Columns' : 'Order Columns'}
+        </Button>
+        {columnOrderView ? (
+          <SelectColumnOrder />
+        ) : (
+          <>
+            <Form.Row>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={onSelectAll}
+                hidden={!props.editable}
+                data-testid="qb-select-all-button"
+              >
+                <i className="material-icons mr-1">check_box</i>
+                Select All
+              </Button>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={onDeselectAll}
+                hidden={!props.editable}
+                data-testid="qb-select-none-button"
+              >
+                <i className="material-icons mr-1">check_box_outline_blank</i>
+                Deselect All
+              </Button>
+            </Form.Row>
+            <SelectColumnValidator step={props.step} steps={props.steps}>
+              <CheckboxGroup
+                options={selectableColumns.sort(sortObjectArrayByProperty('text').sort)}
+                selectedOptions={props.columns}
+                onUpdateOptions={props.onUpdateColumns}
+              />
+            </SelectColumnValidator>
+          </>
+        )}
       </Form.Group>
     </React.Fragment>
   );
