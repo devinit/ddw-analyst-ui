621d3df448b491104f5f88d689ea8144784b76cf
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-04-19 16:43:52 +0300

    Style Column component

--- frontend/src/components/SelectColumn/SelectColumn.tsx
+++ frontend/src/components/SelectColumn/SelectColumn.tsx
@@ -8,12 +8,20 @@ interface SelectColumnProps {
 }
 
 const Column = styled.div`
-  border-style: solid;
-  width: 400px;
-  margin: 2px;
+  margin-top: 4px;
+  margin-bottom: 4px;
   cursor: grab;
-  padding: 5px;
-  border-color: grey;
+  word-wrap: break-word;
+  line-height: 1em;
+  white-space: normal;
+  padding: 0.78571429em 2.1em 0.78571429em 1em;
+  color: rgba(0, 0, 0, 0.87);
+  border: 1px solid rgba(34, 36, 38, 0.15);
+  border-radius: 0.28571429rem;
+  min-height: 2.71428571em;
+  max-width: 500px;
+  min-width: 300px;
+  width: fit-content;
 `;
 
 const SelectColumn: FunctionComponent<SelectColumnProps> = (props) => {
