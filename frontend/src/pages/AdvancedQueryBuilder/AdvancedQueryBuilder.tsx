ccde599ffcf60047099236944062561cb74ab277
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-04 19:14:02 +0300

    Make operation form functional in advanced query builder

--- frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
+++ frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
 import { Dimmer, Loader } from 'semantic-ui-react';
 import { OperationTabContainer } from '../../components/OperationTabContainer';
+import { SourcesContext } from '../../context';
 import { OperationMap } from '../../types/operations';
 import { useOperation, useSources } from '../../utils/hooks';
 
@@ -13,10 +14,31 @@ type QueryBuilderProps = RouteComponentProps<RouterParams>;
 
 const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
   const { id: operationID } = props.match.params;
-  const { loading, operation } = useOperation<OperationMap>(
+  const [operation, setOperation] = useState<OperationMap>();
+  const { loading, operation: pageOperation } = useOperation<OperationMap>(
     operationID ? parseInt(operationID) : undefined,
   );
   const sources = useSources({ limit: 200, offset: 0 });
+
+  useEffect(() => {
+    // the page operation has precedence i.e in the event of editing
+    if (pageOperation) {
+      setOperation(pageOperation);
+    }
+  }, [pageOperation]);
+
+  const onSaveOperation = (preview?: boolean) => {
+    console.log('Saving:', preview);
+  };
+
+  const onDeleteOperation = (ope?: OperationMap) => {
+    console.log('Deleting:', ope);
+  };
+
+  const onUpdateOperation = (ope?: OperationMap) => {
+    console.log('Updating:', ope);
+    setOperation(ope);
+  };
 
   return (
     <Row>
@@ -25,7 +47,16 @@ const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
           <Dimmer active={loading || !sources.count()} inverted>
             <Loader content="Loading" />
           </Dimmer>
-          {!loading && sources.count() ? <OperationTabContainer operation={operation} /> : null}
+          {!loading && sources.count() ? (
+            <SourcesContext.Provider value={{ sources }}>
+              <OperationTabContainer
+                operation={operation}
+                onSave={onSaveOperation}
+                onDelete={onDeleteOperation}
+                onUpdate={onUpdateOperation}
+              />
+            </SourcesContext.Provider>
+          ) : null}
         </React.Fragment>
       </Col>
     </Row>
