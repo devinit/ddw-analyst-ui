8bf1d80904cdd6eea8d294f6f944c34f25c124ee
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-06 13:32:38 +0300

    Refactor AdvancedQueryBuilder class

--- query_builder/advanced.py
+++ query_builder/advanced.py
@@ -1,4 +1,5 @@
 
+# from data.db_manager import fetch_data_only
 import operator
 from pypika.queries import Query, Table
 from pypika import Criterion
@@ -16,20 +17,59 @@ FILTER_MAPPING = {
 
 class AdvancedQueryBuilder:
     def __init__(self, operation=None):
-        pass
+        self.andKey = '$and'
+        self.orKey = '$or'
+
+    def process_config(self, config):
+        source = Source.objects.get(pk=config.get('source'));
+
+        table_name = source.active_mirror_name
+        schema_name = source.schema
+
+        table = Table(table_name, schema=schema_name)
+        query = Query.from_(table)
+
+        if 'filter' in config:
+            filterQuery = self.handle_filter(table, query, config)
+            print(filterQuery)
+
+            # data = fetch_data_only(filterQuery.get_sql())
+            # print(data)
+            return
+
+    def handle_filter(self, table, query, config):
+        filterConfig = config.get('filter')
+        if 'columns' in config and config.get('columns'):
+            selectQuery = self.get_select_query(table, query, config.get('columns'))
+            return self.get_filter_query(table, selectQuery, filterConfig)
+
+        return self.get_filter_query(table, query, filterConfig)
+
+    def get_select_query(self, table, query, columns):
+        return query.select(*[table[column] for column in columns])
+
+    def get_filter_query(self, table, query, config):
+        if self.andKey in config:
+            andConfig = config.get(self.andKey)
+
+            # a way to handle complex filter configs
+            crit = Criterion.all([ self.get_filter_criterion(table, config) for config in andConfig ])
+
+            # sample query
+            return query.where(crit)
 
     def get_filter_criterion(self, table, filter):
-        andKey = '$and'
-        orKey = '$or'
-        if andKey in filter:
-            andConfig = filter.get(andKey)
+        if self.andKey in filter:
+            andConfig = filter.get(self.andKey)
             return Criterion.all([ self.get_filter_criterion(table, config) for config in andConfig ])
 
-        if orKey in filter:
-            orConfig = filter.get(orKey)
+        if self.orKey in filter:
+            orConfig = filter.get(self.orKey)
             return Criterion.any([ self.get_filter_criterion(table, config) for config in orConfig ])
 
         return FILTER_MAPPING[filter.get('comp')](table[filter.get('column')], filter.get('value'))
+
+
 
 # TODO: test code - remove in production
 def get_config():
@@ -47,6 +87,11 @@ def get_config():
                     ],
                 },
             ],
+        },
+        'join': {
+            'source': 5,
+            'type': 'inner',
+            'mapping': [['donor_name', 'donor_name']]
         }
     }
 
@@ -54,24 +99,5 @@ def build_query():
     # TODO: test code - remove in production
     config = get_config()
 
-    source = Source.objects.get(pk=config.get('source'));
-
-    table_name = source.active_mirror_name
-    schema_name = source.schema
-
-    table = Table(table_name, schema=schema_name)
-    query = Query.from_(table)
     builder = AdvancedQueryBuilder()
-
-    if 'filter' in config:
-        filterConfig = config.get('filter')
-        andKey = '$and'
-        if andKey in filterConfig:
-            andConfig = filterConfig.get(andKey)
-
-            # a way to handle complex filter configs
-            crit = Criterion.all([ builder.get_filter_criterion(table, config) for config in andConfig ])
-
-            # sample query
-            q = query.select(table.donor_name, table.agency_name).where(crit)
-            print(q)
+    builder.process_config(config)
