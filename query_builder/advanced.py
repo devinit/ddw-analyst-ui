e2d4815b2444c98a85256cbcf1fb76c8d979b114
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-06 15:01:15 +0300

    Add handling for joins

--- query_builder/advanced.py
+++ query_builder/advanced.py
@@ -1,18 +1,30 @@
 
 # from data.db_manager import fetch_data_only
 import operator
+from pypika.enums import JoinType
 from pypika.queries import Query, Table
 from pypika import Criterion
 
 from core.models import Source
 
 FILTER_MAPPING = {
-    "$lt": operator.lt,
-    "$le": operator.le,
-    "$eq": operator.eq,
-    "$neq": operator.ne,
-    "$gt": operator.gt,
-    "$gte": operator.ge,
+    '$lt': operator.lt,
+    '$le': operator.le,
+    '$eq': operator.eq,
+    '$neq': operator.ne,
+    '$gt': operator.gt,
+    '$gte': operator.ge,
+}
+
+JOIN_MAPPING = {
+    'left': JoinType.left,
+    'right': JoinType.right,
+    'inner': JoinType.inner,
+    'outer': JoinType.outer,
+    'left_outer': JoinType.left_outer,
+    'right_outer': JoinType.right_outer,
+    'full': JoinType.full_outer,
+    'cross': JoinType.cross
 }
 
 class AdvancedQueryBuilder:
@@ -21,29 +33,56 @@ class AdvancedQueryBuilder:
         self.orKey = '$or'
 
     def process_config(self, config):
-        source = Source.objects.get(pk=config.get('source'));
+        table = self.get_source_table(config.get('source'))
+        query = Query.from_(table)
+
+        if 'join' in config:
+            join_query = self.handle_join(table, query, config)
+            print(join_query)
+
+            return
+
+        if 'filter' in config:
+            filter_query = self.handle_filter(table, query, config)
+            print(filter_query)
+
+            return
+
+        # data = fetch_data_only(joinQuery.get_sql())
+        # print(data)
+
+    def get_source_table(self, source_id):
+        source = Source.objects.get(pk=source_id);
 
         table_name = source.active_mirror_name
         schema_name = source.schema
 
-        table = Table(table_name, schema=schema_name)
-        query = Query.from_(table)
-
-        if 'filter' in config:
-            filterQuery = self.handle_filter(table, query, config)
-            print(filterQuery)
-
-            # data = fetch_data_only(filterQuery.get_sql())
-            # print(data)
-            return
+        return Table(table_name, schema=schema_name)
 
     def handle_filter(self, table, query, config):
-        filterConfig = config.get('filter')
+        filter_config = config.get('filter')
         if 'columns' in config and config.get('columns'):
             selectQuery = self.get_select_query(table, query, config.get('columns'))
-            return self.get_filter_query(table, selectQuery, filterConfig)
+            return self.get_filter_query(table, selectQuery, filter_config)
 
-        return self.get_filter_query(table, query, filterConfig)
+        return self.get_filter_query(table, query, filter_config)
+
+    def handle_join(self, table, query, config):
+        join_config = config.get('join')
+        join_table = self.get_source_table(join_config.get('source'))
+
+        join_query = query.join(join_table, JOIN_MAPPING[join_config.get('type')]).on(operator.and_(*[
+            operator.eq(table[mapping[0]], join_table[mapping[1]]) for mapping in join_config.get('mapping')
+        ]))
+        if 'filter' in config:
+            join_query = self.handle_filter(table, join_query, config)
+
+            return join_query
+
+        if 'columns' in config and config.get('columns'):
+            join_query = self.get_select_query(table, join_query, config.get('columns'))
+
+        return join_query
 
     def get_select_query(self, table, query, columns):
         return query.select(*[table[column] for column in columns])
@@ -91,7 +130,7 @@ def get_config():
         'join': {
             'source': 5,
             'type': 'inner',
-            'mapping': [['donor_name', 'donor_name']]
+            'mapping': [['donor_name', 'donor_name'], ['year', 'year']]
         }
     }
 
