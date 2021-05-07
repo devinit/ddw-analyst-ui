2dc13b9ea1994949d6f459a1d4e12faaf62c3764
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-07 15:31:26 +0300

    Add AdvancedSelectQueryBuilder

---
+++ frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const AdvancedSelectQueryBuilder: FunctionComponent = () => {
+  return <div>Select Query Builder Goes Here!</div>;
+};
+
+export { AdvancedSelectQueryBuilder };
