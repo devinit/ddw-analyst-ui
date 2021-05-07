c5263148b681002d66a6dce07a8e3b552e34bc2e
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-07 15:31:26 +0300

    Add AdvancedSelectQueryBuilder

--- frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
+++ frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
@@ -1,17 +1,23 @@
 import React, { FunctionComponent, useState } from 'react';
+import { AdvancedQueryBuilderAction } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
+import { AdvancedSelectQueryBuilder } from '../AdvancedSelectQueryBuilder';
 import { DataSourceSelector } from '../DataSourceSelector';
 import { QueryBuilderActionSelector } from '../QueryBuilderActionSelector';
 
 const QuerySentenceBuilder: FunctionComponent = () => {
   const [source, setSource] = useState<SourceMap>();
+  const [action, setAction] = useState<AdvancedQueryBuilderAction>();
 
   const onSelectSource = (selectedSource: SourceMap) => setSource(selectedSource);
+  const onSelectAction = (selectedAction: AdvancedQueryBuilderAction) => setAction(selectedAction);
+  console.log(action);
 
   return (
     <div>
       <DataSourceSelector source={source} onSelect={onSelectSource} />
-      <QueryBuilderActionSelector />
+      <QueryBuilderActionSelector onSelectAction={onSelectAction} />
+      {action === 'select' ? <AdvancedSelectQueryBuilder /> : null}
     </div>
   );
 };
