b124ba5feddf9f13159ae948266ad1796756c97f
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-13 14:16:34 +0300

    Use ICheck only for checkboxes

--- frontend/src/components/ICheck/ICheck.tsx
+++ frontend/src/components/ICheck/ICheck.tsx
@@ -1,20 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import React, { ChangeEvent, FunctionComponent, ReactNode } from 'react';
 import 'icheck-bootstrap/icheck-bootstrap.css';
+import classNames from 'classnames';
 
 interface ICheckProps { // eslint-disable-line
-  type: 'checkbox' | 'radio';
-  variant?: 'danger' | 'primary' | 'success';
+  variant?: 'danger' | 'primary' | 'success' | 'info';
   id: string;
-  name?: string;
-  label?: string;
-  onChange?: () => void;
+  name: string;
+  label?: ReactNode;
+  checked?: boolean;
+  onChange?: (data: { value: string; checked: boolean }) => void;
+  className?: string;
 }
 
-const ICheck: FunctionComponent<ICheckProps> = (props) => {
+const ICheck: FunctionComponent<ICheckProps> = ({ id, name, label, checked, ...props }) => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (props.onChange) {
+      props.onChange({ checked: event.target.checked, value: name });
+    }
+  };
+
   return (
-    <div className={`icheck-${props.variant}`}>
-      <input type={props.type} id={props.id} name={props.name} />
-      {props.label ? <label htmlFor={props.id}>{props.label}</label> : null}
+    <div className={classNames(`icheck-${props.variant}`, props.className)}>
+      <input type="checkbox" id={id} name={name} onChange={onChange} checked={checked} />
+      {label ? <label htmlFor={id}>{label}</label> : null}
     </div>
   );
 };
