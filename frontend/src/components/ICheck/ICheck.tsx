ecdd0406df887c86334ed54c781ed7215c3007ad
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-13 15:38:45 +0300

    Add ICheckData prop

--- frontend/src/components/ICheck/ICheck.tsx
+++ frontend/src/components/ICheck/ICheck.tsx
@@ -2,13 +2,18 @@ import React, { ChangeEvent, FunctionComponent, ReactNode } from 'react';
 import 'icheck-bootstrap/icheck-bootstrap.css';
 import classNames from 'classnames';
 
+export interface ICheckData { // eslint-disable-line
+  value: string;
+  checked: boolean;
+}
+
 interface ICheckProps { // eslint-disable-line
   variant?: 'danger' | 'primary' | 'success' | 'info';
   id: string;
   name: string;
   label?: ReactNode;
   checked?: boolean;
-  onChange?: (data: { value: string; checked: boolean }) => void;
+  onChange?: (data: ICheckData) => void;
   className?: string;
 }
 
