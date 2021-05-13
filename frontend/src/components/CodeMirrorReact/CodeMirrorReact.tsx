bf51bb1c9b1d3b9b6eecd31ad5316654d495f58a
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-13 10:46:35 +0300

    Add config prop

--- frontend/src/components/CodeMirrorReact/CodeMirrorReact.tsx
+++ frontend/src/components/CodeMirrorReact/CodeMirrorReact.tsx
@@ -1,16 +1,21 @@
 import React, { FunctionComponent, useEffect, useRef } from 'react';
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
-const CodeMirrorReact: FunctionComponent = () => {
+export type JsonModeSpec = {
+  json: boolean;
+};
+
+interface CodeMirrorReactProps {
+  config: CodeMirror.EditorConfiguration;
+}
+
+const CodeMirrorReact: FunctionComponent<CodeMirrorReactProps> = (props) => {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (ref.current) {
-      const codeMirror = CodeMirror(ref.current, {
-        value: JSON.stringify({ mode: 'JSON' }, null, 4),
-        mode: 'json',
-        lineNumbers: true,
-      });
+      const codeMirror = CodeMirror(ref.current, props.config);
       console.log(codeMirror);
     }
   });
--- frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
+++ frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
@@ -2,9 +2,11 @@ import React, { FunctionComponent, useState } from 'react';
 import { AdvancedQueryBuilderAction } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
 import { AdvancedSelectQueryBuilder } from '../AdvancedSelectQueryBuilder';
-import { CodeMirrorReact } from '../CodeMirrorReact';
+import { CodeMirrorReact, JsonModeSpec } from '../CodeMirrorReact';
 import { DataSourceSelector } from '../DataSourceSelector';
 import { QueryBuilderActionSelector } from '../QueryBuilderActionSelector';
+
+const mode: CodeMirror.ModeSpec<JsonModeSpec> = { name: 'javascript', json: true };
 
 const QuerySentenceBuilder: FunctionComponent = () => {
   const [source, setSource] = useState<SourceMap>();
@@ -12,14 +14,15 @@ const QuerySentenceBuilder: FunctionComponent = () => {
 
   const onSelectSource = (selectedSource: SourceMap) => setSource(selectedSource);
   const onSelectAction = (selectedAction: AdvancedQueryBuilderAction) => setAction(selectedAction);
-  console.log(action);
+
+  const defaultValue = { mode: 'JSON' };
 
   return (
     <div>
       <DataSourceSelector source={source} onSelect={onSelectSource} />
       <QueryBuilderActionSelector onSelectAction={onSelectAction} />
       {action === 'select' ? <AdvancedSelectQueryBuilder /> : null}
-      <CodeMirrorReact />
+      <CodeMirrorReact config={{ mode, value: JSON.stringify(defaultValue, null, 2) }} />
     </div>
   );
 };
