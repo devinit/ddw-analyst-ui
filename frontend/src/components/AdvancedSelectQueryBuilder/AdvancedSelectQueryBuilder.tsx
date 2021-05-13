561bd30a4aaec391d93be5abcfd28d0d8908494f
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-13 13:49:48 +0300

    Add Select Columns button

--- frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
+++ frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
@@ -1,7 +1,35 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+// interface ComponentProps {}
 
 const AdvancedSelectQueryBuilder: FunctionComponent = () => {
-  return <div>Select Query Builder Goes Here!</div>;
+  useEffect(() => {
+    (window as any).$('[data-toggle="tooltip"]').tooltip(); // eslint-disable-line
+  }, []);
+
+  return (
+    <div className="mb-3">
+      <ButtonGroup className="mr-2">
+        <Button
+          variant="danger"
+          size="sm"
+          data-toggle="tooltip"
+          data-placement="bottom"
+          data-html="true"
+          title={`<i>Replaces</i> <strong>ALL</strong> columns with those selected`}
+        >
+          Select Column(s)
+        </Button>
+        <Button variant="danger" size="sm" className="d-none">
+          Insert Column
+        </Button>
+        <Button variant="danger" size="sm" className="d-none">
+          Order Columns
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
 };
 
 export { AdvancedSelectQueryBuilder };
