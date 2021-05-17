b8fb918a266eafb118335113c1c4426e2752c1d1
David Ebukali <debukali@gmail.com>
2021-05-17 10:25:14 +0300

    Use available column aliases in query builder feedback messages

--- frontend/src/pages/QueryBuilder/QueryBuilder.tsx
+++ frontend/src/pages/QueryBuilder/QueryBuilder.tsx
@@ -116,12 +116,17 @@ const QueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
     const aliasCreationStatus = operation.get(
       'alias_creation_status',
     ) as AliasCreationStatus | null;
+    const aliases = operation.get('aliases') as Map<string, Map<string, string | number>>;
+    let columnAliases;
+    if (aliases) {
+      columnAliases = aliases.map((alias) => alias.get('column_alias'));
+    }
     if (logs?.get('type') || logs?.get('message')) {
       if (logs.get('message') === OBSOLETE_COLUMNS_LOG_MESSAGE) {
-        const columns = logs.get('columns') as string[];
+        columnAliases = columnAliases ? columnAliases : (logs.get('columns') as string[]);
         const steps = logs.get('steps') as number[];
         setAlertMessages([
-          `Columns ${columns.join(', ')} used in steps ${steps.join(', ')} are obsolete.`,
+          `Columns ${columnAliases.join(', ')} used in steps ${steps.join(', ')} are obsolete.`,
           'NB: This warning will be cleared on save',
         ]);
       }
