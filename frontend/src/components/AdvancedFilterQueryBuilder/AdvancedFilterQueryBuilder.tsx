a75ea702173763731688ef8c24f43e8d65e37ff0
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-24 16:26:32 +0300

    Add editor to filter query builder

--- frontend/src/components/AdvancedFilterQueryBuilder/AdvancedFilterQueryBuilder.tsx
+++ frontend/src/components/AdvancedFilterQueryBuilder/AdvancedFilterQueryBuilder.tsx
@@ -1,16 +1,31 @@
-import React, { FunctionComponent, useContext, useEffect } from 'react';
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import { SourceMap } from '../../types/sources';
-import { AdvancedQueryContext } from '../QuerySentenceBuilder';
+import { CodeMirrorReact } from '../CodeMirrorReact';
+import { AdvancedQueryContext, jsonMode } from '../QuerySentenceBuilder';
 import { validateFilter } from './utils';
 import { handleAnd } from './utils/actions';
 
 interface ComponentProps {
   source: SourceMap;
 }
+const defaultOptions = {
+  $and: [
+    { column: 'donor_name', comp: '$eq', value: 'United States' },
+    { column: 'agency_name', comp: '$eq', value: 'Department of Agriculture' },
+    {
+      $or: [
+        { column: 'agency_name', comp: '$eq', value: 'Department of Agriculture' },
+        { column: 'agency_name', comp: '$eq', value: 'Miscellaneous' },
+      ],
+    },
+  ],
+};
 
 const AdvancedFilterQueryBuilder: FunctionComponent<ComponentProps> = () => {
   const { options, editor } = useContext(AdvancedQueryContext);
+  const [showEditor, setShowEditor] = useState(false);
+  const [editorContent, setEditorContent] = useState<Record<string, unknown>>({});
   useEffect(() => {
     (window as any).$('[data-toggle="tooltip"]').tooltip(); // eslint-disable-line
   }, []);
@@ -19,6 +34,8 @@ const AdvancedFilterQueryBuilder: FunctionComponent<ComponentProps> = () => {
     if (editor) {
       const validationResponse = validateFilter({ action: '$and', options, editor });
       handleAnd(validationResponse);
+      setEditorContent(defaultOptions);
+      setShowEditor(true);
     }
   };
 
@@ -40,6 +57,16 @@ const AdvancedFilterQueryBuilder: FunctionComponent<ComponentProps> = () => {
           Insert OR
         </Button>
       </ButtonGroup>
+      {showEditor ? (
+        <CodeMirrorReact
+          config={{
+            mode: jsonMode,
+            value: JSON.stringify(editorContent, null, 2),
+            lineNumbers: true,
+            theme: 'material',
+          }}
+        />
+      ) : null}
     </div>
   );
 };
