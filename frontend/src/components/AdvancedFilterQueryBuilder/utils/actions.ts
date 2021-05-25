c9d70d8ea0dff53d12d397188f0d3398755a2f9b
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-26 01:29:59 +0300

    Add filter to AND JSON

--- frontend/src/components/AdvancedFilterQueryBuilder/utils/actions.ts
+++ frontend/src/components/AdvancedFilterQueryBuilder/utils/actions.ts
@@ -3,3 +3,12 @@ import { ValidationResponse } from '.';
 export const handleAnd = (validationResponse: ValidationResponse): void => {
   console.log(validationResponse);
 };
+
+export const operations = [
+  { key: '$lt', text: 'is Less Than', value: '$lt' },
+  { key: '$le', text: 'is Less Than Or Equal', value: '$le' },
+  { key: '$eq', text: 'is Equal', value: '$eq' },
+  { key: '$neq', text: 'is Not Equal', value: '$ne' },
+  { key: '$gt', text: 'is Greater Than', value: '$gt' },
+  { key: '$ge', text: 'is Greater Than Or Equal', value: '$ge' },
+];
