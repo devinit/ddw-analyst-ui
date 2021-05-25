c6cff3c2073e3dcaf20fb25c96445182efe52736
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-25 19:35:31 +0300

    Show Alert for invalid JSON

--- frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
+++ frontend/src/components/QuerySentenceBuilder/QuerySentenceBuilder.tsx
@@ -1,6 +1,7 @@
 import CodeMirror from 'codemirror';
 import { fromJS } from 'immutable';
 import React, { createContext, FunctionComponent, useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
 import {
   AdvancedQueryBuilderAction,
   AdvancedQueryOptions,
@@ -37,31 +38,39 @@ const QuerySentenceBuilder: FunctionComponent<ComponentProps> = (props) => {
   const [context, setContext] = useState<QueryContextProps>({
     options: defaultOptions as AdvancedQueryOptions,
   });
-  const onUpdateOptions = (options: Partial<AdvancedQueryOptions>) => {
-    setContext({ options: { ...context.options, ...options }, updateOptions: onUpdateOptions });
-  };
+  const [alert, setAlert] = useState('');
   useEffect(() => {
     setContext({
       options: { ...context.options, source: source?.get('id') as number, columns: [] },
     });
   }, [source]);
 
+  const onUpdateOptions = (options: Partial<AdvancedQueryOptions>) => {
+    setContext({ options: { ...context.options, ...options }, updateOptions: onUpdateOptions });
+  };
   const onEditorInit = (_editor: CodeMirror.Editor) => setEditor(_editor);
   const onSelectSource = (selectedSource: SourceMap) => setSource(selectedSource);
   const onSelectAction = (selectedAction: AdvancedQueryBuilderAction) => setAction(selectedAction);
   const onChange = (value: string) => {
-    const parsedValue = JSON.parse(value);
-    setContext({
-      options: { ...context.options, ...parsedValue },
-      updateOptions: onUpdateOptions,
-    });
-    if (props.operation) {
-      props.onUpdateOperation(
-        props.operation.set('advanced_config' as keyof Operation, parsedValue),
-      );
-    } else {
-      const operation = fromJS({ advanced_config: parsedValue });
-      props.onUpdateOperation(operation);
+    try {
+      const parsedValue = JSON.parse(value);
+      setContext({
+        options: { ...context.options, ...parsedValue },
+        updateOptions: onUpdateOptions,
+      });
+      if (props.operation) {
+        props.onUpdateOperation(
+          props.operation.set('advanced_config' as keyof Operation, parsedValue),
+        );
+      } else {
+        const operation = fromJS({ advanced_config: parsedValue });
+        props.onUpdateOperation(operation);
+      }
+      setAlert('');
+    } catch (error) {
+      if (error.name === 'SyntaxError' && error.message.includes('Unexpected token')) {
+        setAlert(`Invalid JSON: ${error.message}`);
+      }
     }
   };
 
@@ -84,6 +93,9 @@ const QuerySentenceBuilder: FunctionComponent<ComponentProps> = (props) => {
               onInit={onEditorInit}
               onChange={onChange}
             />
+            <Alert show={!!alert} variant="warning" className="mt-2">
+              {alert}
+            </Alert>
           </>
         ) : null}
       </AdvancedQueryContext.Provider>
