ef77be666a7b70b72cf58bc1b90f075882950b1a
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-30 15:27:01 +0300

    Select Join Type

---
+++ frontend/src/components/AdvancedJoinQueryBuilder/utils/index.ts
@@ -0,0 +1,14 @@
+import { DropdownItemProps } from 'semantic-ui-react';
+import { AdvancedQueryOptions } from '../../../types/operations';
+
+export const hasJoinConfig = (options: AdvancedQueryOptions): boolean => !!options.join;
+export const joinTypes: DropdownItemProps[] = [
+  { key: 'inner', text: 'Inner Join', value: 'inner' },
+  { key: 'outer', text: 'Outer Join', value: 'outer' },
+  { key: 'left', text: 'Left Join', value: 'left' },
+  { key: 'right', text: 'Right Join', value: 'right' },
+  { key: 'full', text: 'Full Join', value: 'full' },
+  { key: 'cross', text: 'Cross Join', value: 'cross' },
+  { key: 'left_outer', text: 'Left Outer Join', value: 'left_outer' },
+  { key: 'right_outer', text: 'Right Outer Join', value: 'right_outer' },
+];
