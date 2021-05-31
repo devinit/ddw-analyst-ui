82142aa4a959b9affc60eb7948c81124f1983b0d
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-05-31 22:49:52 +0300

    Finish AdvancedJoinQueryBuilder

---
+++ frontend/src/components/AdvancedJoinColumnsMapper/AdvancedJoinColumnsMapper.tsx
@@ -0,0 +1,91 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import { Dropdown, DropdownItemProps, DropdownProps } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { ColumnList } from '../../types/sources';
+import { getSelectOptionsFromColumns, sortObjectArrayByProperty } from '../../utils';
+
+interface JoinColumnsMapperProps {
+  primaryColumns: DropdownItemProps[];
+  secondaryColumns: ColumnList;
+  onAdd?: (columnMapping: [string, string]) => void;
+}
+
+const StyledCol = styled(Col)`
+  text-align: center;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+export const AdvancedJoinColumnsMapper: FunctionComponent<JoinColumnsMapperProps> = (props) => {
+  const [selectedColumns, setSelectedColumns] = useState<[string, string]>(['', '']);
+  const onSelectColumn = (
+    _event: React.SyntheticEvent<HTMLElement, Event>,
+    data: DropdownProps,
+  ) => {
+    setSelectedColumns(
+      data.name === 'primaryColumn'
+        ? [data.value as string, selectedColumns[1]]
+        : [selectedColumns[0], data.value as string],
+    );
+  };
+
+  const onAdd = () => {
+    if (props.onAdd) {
+      props.onAdd(selectedColumns);
+    }
+  };
+
+  const { primaryColumns, secondaryColumns } = props;
+
+  return (
+    <Row className="mb-1">
+      <Col lg={4} className="my-2">
+        <Dropdown
+          name="primaryColumn"
+          placeholder="Select Column"
+          fluid
+          selection
+          search
+          options={primaryColumns.sort(sortObjectArrayByProperty('text').sort)}
+          onChange={onSelectColumn}
+          data-testid="qb-join-primary-column-select"
+        />
+      </Col>
+
+      <StyledCol md={2}>
+        <i className="material-icons">arrow_forward</i>
+      </StyledCol>
+
+      <Col lg={4} className="my-2">
+        <Dropdown
+          name="secondaryColumn"
+          placeholder="Select Column"
+          fluid
+          selection
+          search
+          options={getSelectOptionsFromColumns(secondaryColumns).sort(
+            sortObjectArrayByProperty('text').sort,
+          )}
+          onChange={onSelectColumn}
+          data-testid="qb-join-secondary-column-select"
+        />
+      </Col>
+
+      <Col lg={1}>
+        {props.onAdd ? (
+          <Button
+            variant="link"
+            className="btn-just-icon"
+            onClick={onAdd}
+            data-testid="qb-join-add-mapping"
+          >
+            <i className="material-icons">add</i>
+          </Button>
+        ) : null}
+      </Col>
+    </Row>
+  );
+};
+
+AdvancedJoinColumnsMapper.defaultProps = {};
