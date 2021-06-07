3bb4a7b4fbfbf0d948ffede37813db09ac29ea83
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-06-07 21:49:00 +0300

    Add QuerySentence preview component

---
+++ frontend/src/components/QuerySentence/QuerySentence.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { FormControl } from 'react-bootstrap';
+import { format } from 'sql-formatter';
+import { OperationMap } from '../../types/operations';
+import { useOperationQuery } from '../../utils/hooks';
+import { CodeMirrorReact } from '../CodeMirrorReact';
+
+interface QuerySentenceProps {
+  operation: OperationMap;
+}
+
+const QuerySentence: FunctionComponent<QuerySentenceProps> = ({ operation }) => {
+  const { loading, query } = useOperationQuery(operation);
+  const [sentence, setSentence] = useState('');
+  useEffect(() => {
+    if (!loading && query) {
+      setSentence(format(query));
+    }
+  }, [loading, query]);
+
+  return (
+    <div className="mt-2">
+      <CodeMirrorReact
+        config={{
+          mode: 'text',
+          value: sentence,
+          // lineNumbers: true,
+          // theme: 'material',
+          readOnly: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export { QuerySentence };
