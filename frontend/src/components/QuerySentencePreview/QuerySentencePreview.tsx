b3681a179e12395164f9137f2d31312b76d6b64f
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-06-08 00:05:56 +0300

    Finish Query Sentence Preview

--- frontend/src/components/QuerySentencePreview/QuerySentencePreview.tsx
+++ frontend/src/components/QuerySentencePreview/QuerySentencePreview.tsx
@@ -1,6 +1,8 @@
-import React, { FunctionComponent, useContext } from 'react';
+import classNames from 'classnames';
+import React, { FunctionComponent, useContext, useState } from 'react';
 import { OperationMap } from '../../types/operations';
 import { CodeMirrorReact } from '../CodeMirrorReact';
+import { ICheckData, IRadio } from '../IRadio';
 import { QuerySentence } from '../QuerySentence';
 import { AdvancedQueryContext, jsonMode } from '../QuerySentenceBuilder';
 
@@ -10,22 +12,50 @@ interface QuerySentencePreviewProps {
   onEditorUpdate?: (value: string) => void;
 }
 
+type PreviewOption = 'config' | 'query';
+
 const QuerySentencePreview: FunctionComponent<QuerySentencePreviewProps> = (props) => {
   const { options } = useContext(AdvancedQueryContext);
+  const [previewOption, setPreviewOption] = useState<PreviewOption>('config');
+  const onRadioChange = (data: ICheckData) => setPreviewOption(data.value as PreviewOption);
 
   return (
     <>
-      <CodeMirrorReact
-        config={{
-          mode: jsonMode,
-          value: JSON.stringify(options, null, 2),
-          lineNumbers: true,
-          theme: 'material',
-        }}
-        onInit={props.onEditorInit}
-        onChange={props.onEditorUpdate}
-      />
-      {props.operation ? <QuerySentence operation={props.operation} /> : null}
+      <div className="mb-2">
+        <IRadio
+          variant="danger"
+          id="config"
+          name="config"
+          label="Show Config"
+          onChange={onRadioChange}
+          inline
+          checked={previewOption === 'config'}
+        />
+        <IRadio
+          variant="danger"
+          id="query"
+          name="query"
+          label="Preview Query"
+          onChange={onRadioChange}
+          inline
+          checked={previewOption === 'query'}
+        />
+      </div>
+      <div className={classNames({ 'd-none': previewOption !== 'config' })}>
+        <CodeMirrorReact
+          config={{
+            mode: jsonMode,
+            value: JSON.stringify(options, null, 2),
+            lineNumbers: true,
+            theme: 'material',
+          }}
+          onInit={props.onEditorInit}
+          onChange={props.onEditorUpdate}
+        />
+      </div>
+      {previewOption === 'query' && props.operation ? (
+        <QuerySentence operation={props.operation} />
+      ) : null}
     </>
   );
 };
