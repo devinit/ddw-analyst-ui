10b2c8df4c7aedc5fc404c2025e5e3ed58058e08
David Ebukali <debukali@gmail.com>
2021-06-21 18:08:44 +0300

    Update filter operations

--- frontend/src/components/AdvancedFilterQueryBuilder/utils/actions.ts
+++ frontend/src/components/AdvancedFilterQueryBuilder/utils/actions.ts
@@ -6,9 +6,9 @@ export const handleAnd = (validationResponse: ValidationResponse): void => {
 
 export const operations = [
   { key: '$lt', text: 'is Less Than', value: '$lt' },
-  { key: '$le', text: 'is Less Than Or Equal', value: '$le' },
+  { key: '$le', text: 'is Less Than Or Equal', value: '$lte' },
   { key: '$eq', text: 'is Equal', value: '$eq' },
-  { key: '$neq', text: 'is Not Equal', value: '$ne' },
+  { key: '$neq', text: 'is Not Equal', value: '$neq' },
   { key: '$gt', text: 'is Greater Than', value: '$gt' },
-  { key: '$ge', text: 'is Greater Than Or Equal', value: '$ge' },
+  { key: '$ge', text: 'is Greater Than Or Equal', value: '$gte' },
 ];
