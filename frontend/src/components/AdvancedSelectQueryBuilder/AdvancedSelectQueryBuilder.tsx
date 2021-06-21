4a0c50aeb0a342cc384265a08da0f33a818094ef
ThatcherK <kaliithatmau@gmsil.com>
2021-06-21 16:38:26 +0300

    Refactor select actions components

--- frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
+++ frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
@@ -1,9 +1,8 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import { AdvancedQueryColumn } from '../../types/operations';
-import { ColumnList, SourceMap } from '../../types/sources';
+import { SourceMap } from '../../types/sources';
 import { AdvancedQueryContext } from '../QuerySentenceBuilder';
-import { SelectColumnOrder } from '../SelectColumnOrder';
 import { ColumnReset } from './ColumnReset';
 import { ColumnSelector } from './ColumnSelector';
 import { SelectAllColumnSelector } from './SelectAllColumnSelector';
@@ -16,28 +15,10 @@ interface ComponentProps {
 
 const AdvancedSelectQueryBuilder: FunctionComponent<ComponentProps> = ({ source }) => {
   const [displayColumnSelector, setDisplayColumnSelector] = useState(false);
-  const [selectAllColumns, setAllColumns] = useState(
-    (source.get('columns') as ColumnList).toJS().map((column) => {
-      return { alias: column.alias, columnName: column.name };
-    }),
-  );
-  const [displayColumnOrder, setDisplayColumnOrder] = useState(false);
   // const [config, setConfig] = useState<AdvancedQueryOptions>();
   useEffect(() => {
     (window as any).$('[data-toggle="tooltip"]').tooltip(); // eslint-disable-line
   }, []);
-  const updateColumns = (options: string) => {
-    setAllColumns(
-      JSON.parse(options).columns.map((column: unknown) => {
-        return {
-          alias: (source.get('columns') as ColumnList)
-            .toJS()
-            .find((sourceColumn) => sourceColumn.name === column).alias,
-          columnName: column,
-        };
-      }),
-    );
-  };
 
   return (
     <AdvancedQueryContext.Consumer>
@@ -47,30 +28,22 @@ const AdvancedSelectQueryBuilder: FunctionComponent<ComponentProps> = ({ source 
             setShowColumnSelector={setDisplayColumnSelector}
             selectAll={options.selectAll}
             onUpdateOptions={updateOptions}
-            displayColumnOrder={setDisplayColumnOrder}
           />
           <ButtonGroup className="mr-2">
-            {options.selectAll ? (
-              <Button variant="danger" size="sm" onClick={() => setDisplayColumnOrder(true)}>
-                Order All Columns
-              </Button>
-            ) : (
-              <Button
-                variant="danger"
-                size="sm"
-                data-toggle="tooltip"
-                data-placement="top"
-                data-html="true"
-                title={`<i>Replaces</i> <strong>ALL</strong> columns with those selected`}
-                onClick={() => setDisplayColumnSelector(true)}
-              >
-                Select Column(s)
-              </Button>
-            )}
+            <Button
+              variant="danger"
+              size="sm"
+              data-toggle="tooltip"
+              data-placement="top"
+              data-html="true"
+              title={`<i>Replaces</i> <strong>ALL</strong> columns with those selected`}
+              onClick={() => setDisplayColumnSelector(true)}
+            >
+              {options.selectAll ? 'Select Columns for Ordering' : 'Select Column(s)'}
+            </Button>
             <ColumnReset
               onUpdateOptions={updateOptions}
               setDisplayColumnSelector={setDisplayColumnSelector}
-              setDisplayColumnOrder={setDisplayColumnOrder}
             />
             <Button variant="danger" size="sm" className="d-none">
               Insert Column
@@ -83,9 +56,6 @@ const AdvancedSelectQueryBuilder: FunctionComponent<ComponentProps> = ({ source 
             onUpdateSelection={updateOptions}
             selectAll={options.selectAll}
           />
-          {displayColumnOrder ? (
-            <SelectColumnOrder selectedColumns={selectAllColumns} onUpdateColumns={updateColumns} />
-          ) : null}
         </div>
       )}
     </AdvancedQueryContext.Consumer>
