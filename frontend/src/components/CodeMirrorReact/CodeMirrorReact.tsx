4a62836932d6f9fa6201e1432316231ca1677826
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-01 15:48:22 +0300

    Move JSHINT validator to CodeMirrorReact

--- frontend/src/components/CodeMirrorReact/CodeMirrorReact.tsx
+++ frontend/src/components/CodeMirrorReact/CodeMirrorReact.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import CodeMirror from 'codemirror';
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
+import { validateJSHINT } from './utils';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 
@@ -13,17 +14,33 @@ interface CodeMirrorReactProps {
   onChange?: (value?: string | CodeMirror.Doc) => void;
   onInit?: (editor: CodeMirror.Editor) => void;
   className?: string;
+  validateWith?: 'jshint';
 }
+
+const getConfig = (
+  config: CodeMirror.EditorConfiguration,
+  options?: { validate?: boolean },
+): CodeMirror.EditorConfiguration => {
+  if (options) {
+    if (options.validate) {
+      return config.gutters ? config : { ...config, gutters: ['CodeMirror-lint-markers'] };
+    }
+  }
+
+  return config;
+};
 
 const CodeMirrorReact: FunctionComponent<CodeMirrorReactProps> = (props) => {
   const [editor, setEditor] = useState<CodeMirror.Editor>();
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (ref.current) {
-      const codeMirror = CodeMirror(ref.current, props.config);
+      const config = getConfig(props.config, { validate: !!props.validateWith });
+      const codeMirror = CodeMirror(ref.current, config);
       setEditor(codeMirror);
       if (props.onInit) props.onInit(codeMirror);
       codeMirror.on('change', (mirror) => {
+        if (props.validateWith === 'jshint') validateJSHINT(mirror);
         if (props.onChange) props.onChange(mirror.getValue());
       });
     }
---
+++ frontend/src/components/CodeMirrorReact/utils/validate.ts
@@ -0,0 +1,38 @@
+import CodeMirror, { LineWidget } from 'codemirror';
+import { JSHINT } from 'jshint';
+
+const widgets: LineWidget[] = [];
+
+/**
+ * validate editor content with JSHINT validator & display errors
+ * @param editor - CodeMirror.Editor
+ */
+export const validateJSHINT = (editor: CodeMirror.Editor): void => {
+  clearLintingErrors();
+  editor.operation(function () {
+    widgets.forEach((widget) => editor.removeLineWidget(widget));
+    widgets.length = 0;
+    JSHINT(editor.getValue());
+    JSHINT.errors.forEach((error) => {
+      if (!error) return;
+      const alert = document.createElement('div');
+      alert.classList.add(...['alert', 'alert-danger', 'alert-sm', 'pt-1', 'pb-1']);
+
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = error.reason;
+      alert.appendChild(paragraph);
+      widgets.push(
+        editor.addLineWidget(error.line - 1, alert, { coverGutter: false, noHScroll: true }),
+      );
+    });
+  });
+  const info = editor.getScrollInfo();
+  const after = editor.charCoords({ line: editor.getCursor().line + 1, ch: 0 }, 'local').top;
+  if (info.top + info.clientHeight < after) editor.scrollTo(null, after - info.clientHeight + 3);
+};
+
+export const clearLintingErrors = (): void => {
+  for (let index = 0; index < widgets.length; index++) {
+    widgets[index].clear();
+  }
+};
