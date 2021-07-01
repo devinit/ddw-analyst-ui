b366d1158bff6d5da486bdf122db0a5e44a24713
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-01 11:37:44 +0300

    Show only active clause options in the Clause Config

--- frontend/src/components/QuerySentencePreview/utils/index.ts
+++ frontend/src/components/QuerySentencePreview/utils/index.ts
@@ -1,4 +1,4 @@
-import { AdvancedQueryOptions } from '../../../types/operations';
+import { AdvancedQueryBuilderAction, AdvancedQueryOptions } from '../../../types/operations';
 
 /**
  * Returns an empty string for valid options
@@ -9,4 +9,20 @@ export const validateOptions = (options: AdvancedQueryOptions): string => {
   if (!options.source) return 'A data source is required';
 
   return '';
+};
+
+export const getClauseOptions = (
+  options: AdvancedQueryOptions,
+  clause?: AdvancedQueryBuilderAction,
+): Partial<AdvancedQueryOptions> => {
+  if (clause === 'select') {
+    const { selectall, columns, groupby } = options;
+
+    return { selectall, columns, groupby };
+  }
+  if (clause === 'filter') return { filter: options.filter };
+  if (clause === 'join') return { join: options.join };
+  if (clause === 'groupby') return { groupby: options.groupby };
+
+  return options;
 };
