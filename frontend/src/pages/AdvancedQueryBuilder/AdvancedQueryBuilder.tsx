fda2f7264f0ba4f3ab56367b661b71fe03fb25f1
ThatcherK <kaliithatmau@gmail.com>
2021-07-09 09:29:25 +0300

    Make api call to save dataset

--- frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
+++ frontend/src/pages/AdvancedQueryBuilder/AdvancedQueryBuilder.tsx
@@ -1,17 +1,46 @@
+import axios, { AxiosResponse } from 'axios';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { RouteComponentProps } from 'react-router-dom';
+import { connect, MapDispatchToProps } from 'react-redux';
+import { RouteComponentProps, useHistory } from 'react-router-dom';
 import { Dimmer, Loader } from 'semantic-ui-react';
 import { OperationTabContainer } from '../../components/OperationTabContainer';
 import { QuerySentenceBuilder } from '../../components/QuerySentenceBuilder';
 import { SourcesContext } from '../../context';
-import { OperationMap } from '../../types/operations';
+import { ReduxStore } from '../../store';
+import { Operation, OperationMap } from '../../types/operations';
+import { api } from '../../utils';
 import { useOperation, useSources } from '../../utils/hooks';
+import { bindActionCreators } from 'redux';
+import * as pageActions from './actions';
+import * as sourcesActions from '../../actions/sources';
+import { AdvancedQueryBuilderState, queryBuilderReducerId } from './reducers';
+import { UserState } from '../../reducers/user';
+import { TokenState } from '../../reducers/token';
+import { SourceMap } from '../../types/sources';
+import { List } from 'immutable';
+// import { deleteOperation, fetchOperation, setOperation } from '../../actions/operations';
 
 type RouterParams = {
   id?: string;
 };
-type QueryBuilderProps = RouteComponentProps<RouterParams>;
+interface ReduxState {
+  source?: SourceMap;
+  operations: List<OperationMap>;
+  activeOperation?: OperationMap;
+  token: TokenState;
+  page: AdvancedQueryBuilderState;
+  user: UserState;
+}
+interface ActionProps {
+  actions: typeof sourcesActions &
+    typeof pageActions & {
+      fetchOperation: typeof fetchOperation;
+      setActiveOperation: typeof setOperation;
+      deleteOperation: typeof deleteOperation;
+    };
+}
+type QueryBuilderProps = RouteComponentProps & ReduxState<RouterParams>;
 
 const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
   const { id: operationID } = props.match.params;
@@ -20,6 +49,7 @@ const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
     operationID ? parseInt(operationID) : undefined,
   );
   const sources = useSources({ limit: 200, offset: 0 });
+  const history = useHistory();
 
   useEffect(() => {
     // the page operation has precedence i.e in the event of editing
@@ -30,6 +60,50 @@ const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
 
   const onSaveOperation = (preview?: boolean) => {
     console.log('Saving:', preview);
+    if (!operation) {
+      console.log('no operation');
+
+      return;
+    }
+    const url = api.routes.DATASETS;
+    const data: Operation = operation.toJS() as Operation;
+    console.log(props.token);
+    if (props.token) {
+      axios
+        .request({
+          url,
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${props.token}`,
+          },
+          data,
+        })
+        .then((response: AxiosResponse<Operation>) => {
+          props.actions.operationSaved(true);
+          console.log('done');
+          // if (response.status === 200 || response.status === 201) {
+          //   // props.actions.operationSaved(true);
+          //   if (preview) {
+          //     history.push(`/queries/data/${response.data.id}/`);
+          //   } else {
+          //     history.push('/');
+          //   }
+          // }
+        })
+        .catch((error) => {
+          console.log(failed);
+          props.actions.operationSaved(false);
+          // if (error.response.data.error_code === 'e') {
+          //   setAlertMessages([
+          //     'An error occured while creating column aliases for this dataset. Please contact your administrator',
+          //   ]);
+
+          //   return;
+          // }
+          // setAlertMessages(['An unknown error occured - please contact your administrator']);
+        });
+    }
   };
 
   const onDeleteOperation = (ope?: OperationMap) => {
@@ -65,4 +139,27 @@ const AdvancedQueryBuilder: FunctionComponent<QueryBuilderProps> = (props) => {
   );
 };
 
-export default AdvancedQueryBuilder;
+const mapDispatchToProps: MapDispatchToProps<ActionProps, Record<string, unknown>> = (
+  dispatch,
+): ActionProps => ({
+  actions: bindActionCreators(
+    {
+      ...sourcesActions,
+      ...pageActions,
+    },
+    dispatch,
+  ),
+});
+const mapStateToProps = (reduxStore: ReduxStore): ReduxState => {
+  return {
+    token: reduxStore.get('token') as TokenState,
+    operations: reduxStore.getIn(['operations', 'operations']),
+    activeOperation: reduxStore.getIn(['operations', 'activeOperation']),
+    page: reduxStore.get(`${queryBuilderReducerId}`),
+    user: reduxStore.get('user') as UserState,
+  };
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps)(AdvancedQueryBuilder);
+
+export { connector as default };
