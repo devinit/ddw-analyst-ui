b07ea61345d6a318ef278f1da1fabd9ddedff6c7
ThatcherK <kaliithatmau@gmail.com>
2021-07-09 09:29:25 +0300

    Make api call to save dataset

---
+++ frontend/src/pages/AdvancedQueryBuilder/actions/index.ts
@@ -0,0 +1,12 @@
+import {
+  AdvancedQueryBuilderAction,
+  SAVING_OPERATION,
+  SAVING_OPERATION_FAILED,
+  SAVING_OPERATION_SUCCESS,
+} from '../reducers';
+
+export const savingOperation = (): Partial<AdvancedQueryBuilderAction> => ({
+  type: SAVING_OPERATION,
+});
+export const operationSaved = (saved: boolean): Partial<AdvancedQueryBuilderAction> =>
+  saved ? { type: SAVING_OPERATION_SUCCESS } : { type: SAVING_OPERATION_FAILED };
