1a9bbc042a107af60b4bb0a262b125b0189ab0cd
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-13 19:10:27 +0300

    Update aggregate button conditional visibility

--- frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
+++ frontend/src/components/AdvancedSelectQueryBuilder/AdvancedSelectQueryBuilder.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
-import { SourceMap } from '../../types/sources';
+import { AdvancedQueryColumn, AdvancedQueryOptions } from '../../types/operations';
+import { ColumnList, SourceMap } from '../../types/sources';
 import { ICheck, ICheckData } from '../ICheck';
 import { AdvancedQueryContext } from '../QuerySentenceBuilder';
 import { AdvancedQueryBuilderColumnOrder } from './AdvancedQueryBuilderColumnOrder/AdvancedQueryBuilderColumnOrder';
@@ -21,26 +22,57 @@ const getDefaultSelectAll = (usage: AdvancedSelectUsage, selectAll?: boolean): b
   return typeof selectAll !== 'undefined' ? selectAll : true;
 };
 
+const hasNumericalColumn = (
+  sourceColumns: ColumnList,
+  activeColumns: AdvancedQueryColumn[],
+): boolean =>
+  !!activeColumns.find((_column) =>
+    sourceColumns.find(
+      (column) => column.get('data_type') === 'N' && column.get('name') === _column.name,
+    ),
+  );
+
+const showAggregateButton = (
+  sourceColumns: ColumnList,
+  options: AdvancedQueryOptions,
+  usage: AdvancedSelectUsage = 'select',
+) => {
+  if (usage === 'join' && options.join?.columns && options.join.columns.length) {
+    return hasNumericalColumn(sourceColumns, options.join.columns);
+  }
+
+  if (usage === 'select' && options.columns && options.columns.length) {
+    return hasNumericalColumn(sourceColumns, options.columns);
+  }
+
+  return false;
+};
+
 const AdvancedSelectQueryBuilder: FunctionComponent<ComponentProps> = ({ source, usage }) => {
   const { options, updateOptions } = useContext(AdvancedQueryContext);
   const [activeAction, setActiveAction] = useState<'select' | 'order' | 'aggregate'>();
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const [selectAll, setSelectAll] = useState(getDefaultSelectAll(usage!, options.selectall));
   useEffect(() => {
     (window as any).$('[data-toggle="tooltip"]').tooltip(); // eslint-disable-line
     if (typeof options.selectall === 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       updateOptions!({ selectall: true });
     }
   }, []);
   const onToggleSelectAll = (data: ICheckData) => {
     setSelectAll(data.checked);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     updateOptions!({ selectall: data.checked });
   };
   const onReset = () => {
     setActiveAction(undefined);
     if (usage === 'select') {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       updateOptions!({ selectall: true, columns: [] });
     } else if (options.join && options.join.columns) {
       delete options.join.columns;
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       updateOptions!({ join: options.join });
     }
   };
@@ -85,10 +117,7 @@ const AdvancedSelectQueryBuilder: FunctionComponent<ComponentProps> = ({ source,
           variant="danger"
           size="sm"
           hidden={
-            selectAll ||
-            (usage === 'join' && options.join
-              ? !options.join.columns || !options.join.columns.length
-              : !options.columns || !options.columns.length)
+            selectAll || !showAggregateButton(source.get('columns') as ColumnList, options, usage)
           }
           onClick={() => setActiveAction('aggregate')}
         >
