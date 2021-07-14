c18d9c0072d432310df783d9f2843167892ee371
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-14 22:26:44 +0300

    Fix filter validation

--- frontend/src/components/AdvancedFilterQueryBuilder/utils/validate.ts
+++ frontend/src/components/AdvancedFilterQueryBuilder/utils/validate.ts
@@ -1,27 +1,46 @@
-import { fromJS } from 'immutable';
 import { DropdownItemProps } from 'semantic-ui-react';
 import { AdvancedQueryFilter, comp } from '../../../types/operations';
 import { EditorContent } from '../FilterWithAndOr';
 
 export type ValidationResponse = 'invalid' | 'create' | 'update';
 
+const comparatorKeys = ['$and', '$or'];
+const validKeys = ['column', 'comp', 'value'].concat(comparatorKeys);
+
 export const validateFilter = (
   filter: AdvancedQueryFilter,
   columns: DropdownItemProps[],
-): string[] => [
-  ...validateOperators(filter),
-  ...validateColumns(filter, columns),
-  ...validateAndOr(filter),
-];
+): string[] => [...validateOperators(filter), ...validateColumns(filter, columns)];
 
-export const validateOperators = (filterOptions: AdvancedQueryFilter): string[] => {
-  const messages: string[] = [];
+const validateOperators = (filterOptions: AdvancedQueryFilter): string[] => {
+  let messages: string[] = [];
 
   for (const key in filterOptions) {
-    const filter = (filterOptions as any)[key]; // eslint-disable-line @typescript-eslint/no-explicit-any
-    for (const index in filter) {
-      if (!comp.includes(filter[index]['comp'])) {
-        messages.push(`The operator ${filter[index]['comp']} is invalid.`);
+    if (!comparatorKeys.includes(key)) {
+      messages.push(`${key} is not a valid comparator`);
+    } else {
+      const filter = (filterOptions as any)[key]; // eslint-disable-line @typescript-eslint/no-explicit-any
+      for (const index in filter) {
+        const filterKeys = Object.keys(filter[index]);
+        // check if this is a deeper nested filter
+        if (filterKeys.filter((f) => comparatorKeys.includes(f)).length) {
+          messages = messages.concat(validateOperators(filter[index]));
+        } else {
+          // validate keys
+          filterKeys.forEach((key) => {
+            if (!validKeys.includes(key)) {
+              messages.push(`Key ${key} is invalid`);
+            }
+          });
+          // validate operator
+          const operator = filter[index]['comp'];
+          if (operator && !comp.includes(operator)) {
+            messages.push(`The operator ${operator} is invalid.`);
+          }
+          if (!operator) {
+            messages.push('Operator is required');
+          }
+        }
       }
     }
   }
@@ -29,17 +48,23 @@ export const validateOperators = (filterOptions: AdvancedQueryFilter): string[] 
   return messages.length > 0 ? messages : [];
 };
 
-export const validateColumns = (
+const validateColumns = (
   filterOptions: AdvancedQueryFilter,
   columns: DropdownItemProps[],
 ): string[] => {
-  let selectedColumns: string[] = [];
+  const selectedColumns: string[] = [];
   const allColumns: string[] = [];
+  let messages: string[] = [];
 
   for (const key in filterOptions) {
     const filter = (filterOptions as any)[key]; // eslint-disable-line @typescript-eslint/no-explicit-any
     for (const index in filter) {
-      selectedColumns.push(filter[index]['column']);
+      const filterKeys = Object.keys(filter[index]);
+      if (filterKeys.filter((f) => comparatorKeys.includes(f)).length) {
+        messages = messages.concat(validateColumns(filter[index], columns));
+      } else {
+        selectedColumns.push(filter[index]['column']);
+      }
     }
   }
 
@@ -47,23 +72,13 @@ export const validateColumns = (
     allColumns.push(columns[key].value as string);
   }
 
-  selectedColumns = selectedColumns
-    .filter((column) => !allColumns.includes(column))
-    .map((column) => (column ? `${column} is not a valid column name.` : 'column is required'));
+  messages = messages.concat(
+    selectedColumns
+      .filter((column) => !allColumns.includes(column))
+      .map((column) => (column ? `${column} is not a valid column name.` : 'column is required')),
+  );
 
-  return selectedColumns;
-};
-
-export const validateAndOr = (content: EditorContent): string[] => {
-  const validKeys = ['$and', '$or'];
-  const invalidKeys: string[] = [];
-  fromJS(content).mapKeys((key: string) => {
-    if (!validKeys.includes(key)) {
-      invalidKeys.push(`${key} is not a valid comparator`);
-    }
-  });
-
-  return invalidKeys;
+  return messages;
 };
 
 export const isEditorContentEmpty = (obj: EditorContent): boolean => {
