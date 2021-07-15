4b5aeae48e118487eb3fd812eb715b25717ebfa3
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-15 18:30:04 +0300

    Refactor saveOperation function

--- frontend/src/pages/AdvancedQueryBuilder/utils/operations.ts
+++ frontend/src/pages/AdvancedQueryBuilder/utils/operations.ts
@@ -5,34 +5,28 @@ import { Operation, OperationMap } from '../../../types/operations';
 import { api, localForageKeys } from '../../../utils';
 import { clearOperationsCache } from '../../../utils/cache';
 
-export const saveOperation = (operation: OperationMap, history: History): void => {
+export const saveOperation = async (operation: OperationMap): Promise<void> => {
   if (!operation) {
     return;
   }
   const id = operation.get('id');
   const url = id ? `${api.routes.SINGLE_DATASET}${id}/` : api.routes.DATASETS;
   const data: Operation = operation.toJS() as Operation;
-  localForage.getItem<string>(localForageKeys.API_KEY).then((token) => {
-    axios
-      .request({
-        url,
-        method: id ? 'put' : 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `token ${token}`,
-        },
-        data,
-      })
-      .then((response: AxiosResponse<Operation>) => {
-        if (response.status === 200 || response.status === 201) {
-          clearOperationsCache();
-          history.push('/');
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const token = await localForage.getItem<string>(localForageKeys.API_KEY);
+  const response = await axios.request<Operation>({
+    url,
+    method: id ? 'put' : 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `token ${token}`,
+    },
+    data,
   });
+  if (response.status === 200 || response.status === 201) {
+    clearOperationsCache();
+  }
+
+  return;
 };
 
 export const deleteOperation = (operationID: string, history: History): void => {
