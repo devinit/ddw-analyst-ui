cb85a40157ac8125b2fff8d172e9c6d528b279d9
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-07-15 18:55:23 +0300

    Add AppContext for user and token access

--- frontend/src/context/index.ts
+++ frontend/src/context/index.ts
@@ -1,5 +1,8 @@
 import { SourceMap } from '../types/sources';
 import { List } from 'immutable';
 import { createContext } from 'react';
+import { UserState } from '../reducers/user';
+import { TokenState } from '../reducers/token';
 
+export const AppContext = createContext<Partial<{ user: UserState; token: TokenState }>>({});
 export const SourcesContext = createContext<{ sources: List<SourceMap> }>({ sources: List() });
