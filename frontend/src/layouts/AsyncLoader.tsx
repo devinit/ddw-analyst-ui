1bda58d2771d0399c572d670371d834df9239d7f
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-09-21 12:35:11 +0300

    Fix layout AsyncLoader

--- frontend/src/layouts/AsyncLoader.tsx
+++ frontend/src/layouts/AsyncLoader.tsx
@@ -3,7 +3,9 @@ import { RouteComponentProps } from 'react-router-dom';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
 const MainLayout = React.lazy(() => import('./MainLayout'));
-export const AsyncMainLayout = (props: RouteComponentProps): ReactElement<SuspenseProps> => (
+export const AsyncMainLayout = (
+  props: RouteComponentProps & { loading: boolean },
+): ReactElement<SuspenseProps> => (
   <Suspense
     fallback={
       <Dimmer active={true} inverted>
