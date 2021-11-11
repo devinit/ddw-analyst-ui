fecb6728beaf76ae121d65b1b74fc68641469e84
Wakibi Chris <wakibic@gmail.com>
2021-11-11 17:16:06 +0300

    Remove working directory change

--- certbot_success.sh
+++ certbot_success.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-working_directory=/root/ddw-analyst-ui
-#If successful copy certs to the correct folder and refresh nginx docker node
-cd $working_directory
 
 cp -f /etc/letsencrypt/live/"${SITE_URL}"/privkey.pem /etc/letsencrypt/privkey.pem
 cp -f /etc/letsencrypt/live/"${SITE_URL}"/fullchain.pem /etc/letsencrypt/fullchain.pem
