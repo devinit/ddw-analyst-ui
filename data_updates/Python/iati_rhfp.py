9d03e6343a361e49ab499c789c4fc197826bec2b
Wakibi Chris <wakibic@gmail.com>
2022-02-14 17:05:57 +0300

    Add fn to push to Github

--- data_updates/Python/iati_rhfp.py
+++ data_updates/Python/iati_rhfp.py
@@ -1,7 +1,42 @@
 import os
 import requests
+import glob
 from requests.auth import HTTPBasicAuth
 import pandas as pd
+from github import Github, InputGitTreeElement
+from django.conf import settings
+
+
+GITHUB_KEY = 'you_can_put_your_github_token_here_for_testing'
+
+def push_folder_to_github(repo_name, branch, local_folder, remote_folder, commit_msg, file_extension='*.*'):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    glob_path = os.path.join(dir_path, local_folder, file_extension)
+    csv_paths = glob.glob(glob_path)
+    abs_csv_paths = [os.path.abspath(csv) for csv in csv_paths]
+    if len(abs_csv_paths) < 1:
+        print('Nothing to push')
+        return
+    g = Github(settings.GITHUB_TOKEN)
+    # Uncomment below line to use your token for testing. Make sure you do not push changes with your token to Github
+    # g = Github(GITHUB_KEY)
+    repo = g.get_repo(repo_name)
+    git_ref = repo.get_git_ref('heads/' + branch)
+    branch_obj = repo.get_branch(branch=branch)
+    git_sha = branch_obj.commit.sha
+    base_tree = repo.get_git_tree(git_sha)
+    element_list = list()
+    for abs_csv_path in abs_csv_paths:
+        with open(abs_csv_path) as input_file:
+            data = input_file.read()
+        remote_file = remote_folder + '/' + os.path.basename(abs_csv_path)
+        element = InputGitTreeElement(remote_file, '100644', 'blob', data)
+        element_list.append(element)
+
+    tree = repo.create_git_tree(element_list, base_tree)
+    parent = repo.get_git_commit(git_sha)
+    commit = repo.create_git_commit(commit_msg, tree, [parent])
+    git_ref.edit(commit.sha)
 
 
 print("Starting read-in")
@@ -35,3 +70,6 @@ data.loc[data['Transaction Receiver Organisation Type'].isin([90]),'Transaction 
 data["x_transaction_value_usd_m_Sum"] = data["x_transaction_value_usd_m_Sum"]/1000000
 
 data.to_csv("iati_rhfp.csv", encoding='utf-8', index=False)
+
+# Sample call to the function below
+# push_folder_to_github('wakibi/di-website-data', 'main', 'csv', '2022', 'Committing from API', '*.csv')
