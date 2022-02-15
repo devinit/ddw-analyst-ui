ca96e8e8e13665f153c4e538598cf37e72c2770b
devinit <kaliithatmau@gmail.com>
2022-02-15 11:55:51 +0300

    Add aggregate column check to HavingQueryBuilder

--- frontend/src/components/AdvancedHavingQueryBuilder/AdvancedHavingQueryBuilder.tsx
+++ frontend/src/components/AdvancedHavingQueryBuilder/AdvancedHavingQueryBuilder.tsx
@@ -1,21 +1,29 @@
 import React, { FunctionComponent, useContext } from 'react';
 import { Alert } from 'react-bootstrap';
+import { AdvancedQueryColumn } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
 import { AdvancedQueryContext } from '../QuerySentenceBuilder';
 
 interface ComponentProps {
   source: SourceMap;
 }
-const AdvancedHavingQueryBuilder: FunctionComponent<ComponentProps> = ({ source }) => {
+const AdvancedHavingQueryBuilder: FunctionComponent<ComponentProps> = () => {
   const { options } = useContext(AdvancedQueryContext);
+  const hasAggregate = (columns: AdvancedQueryColumn[]) => {
+    const aggregateColumns = columns.find((column: AdvancedQueryColumn) => column.aggregate);
+    if (aggregateColumns) return true;
+
+    return false;
+  };
 
   return (
     <>
-      {(options.groupby as string[])?.length > 0 ? (
+      {(options.groupby as string[])?.length > 0 ||
+      hasAggregate(options.columns as AdvancedQueryColumn[]) ? (
         <div>Having block goes here</div>
       ) : (
         <Alert variant="warning" className="mt-2">
-          Having close requires groupBy columns
+          Having clause requires or aggregate columns
         </Alert>
       )}
     </>
