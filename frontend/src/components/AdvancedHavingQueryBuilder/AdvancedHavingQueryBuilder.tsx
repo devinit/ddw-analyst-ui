2c4f75f70bb4e14d5bb14ce4f4d7dca424ac6a1b
devinit <kaliithatmau@gmail.com>
2022-02-17 17:42:00 +0300

    Add group columns to JqueryQueryBuilder

--- frontend/src/components/AdvancedHavingQueryBuilder/AdvancedHavingQueryBuilder.tsx
+++ frontend/src/components/AdvancedHavingQueryBuilder/AdvancedHavingQueryBuilder.tsx
@@ -1,7 +1,9 @@
-import React, { FunctionComponent, useContext } from 'react';
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import { Alert } from 'react-bootstrap';
-import { AdvancedQueryColumn } from '../../types/operations';
+import { AdvancedQueryColumn, JqueryQueryBuilderFieldData } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
+import { JqueryQueryBuilder } from '../JqueryQueryBuilder';
+import { createQueryBuilderRules } from '../JqueryQueryBuilder/utils';
 import { AdvancedQueryContext } from '../QuerySentenceBuilder';
 
 interface ComponentProps {
@@ -9,8 +11,37 @@ interface ComponentProps {
 }
 const AdvancedHavingQueryBuilder: FunctionComponent<ComponentProps> = () => {
   const { options } = useContext(AdvancedQueryContext);
+  const [jqBuilder, setJqBuilder] = useState<any>({});
+
+  const getGroupColumns = () =>
+    options.columns?.filter((col) => options.groupby?.includes(col.name as string));
+
+  const getColumns = () => {
+    if (options.groupby) {
+      const groupColumns = getGroupColumns();
+
+      return groupColumns;
+    }
+  };
+
+  const fieldData: JqueryQueryBuilderFieldData[] = (getColumns() as AdvancedQueryColumn[]).map(
+    (column) => {
+      return {
+        id: column.name as string,
+        label: column.alias as string,
+        type: 'string',
+        operators: ['equal', 'less', 'greater'],
+      };
+    },
+  );
+
+  const getJqueryBuilderInstance = (jqInstance: any) => {
+    setJqBuilder(jqInstance);
+  };
+  // const getAggregateColumns = () => options.columns?.filter((column) => column.aggregate);
+
   const hasAggregate = (columns: AdvancedQueryColumn[]) => {
-    const aggregateColumns = columns.find((column: AdvancedQueryColumn) => column.aggregate);
+    const aggregateColumns = columns?.find((column: AdvancedQueryColumn) => column.aggregate);
     if (aggregateColumns) return true;
 
     return false;
@@ -20,10 +51,21 @@ const AdvancedHavingQueryBuilder: FunctionComponent<ComponentProps> = () => {
     <>
       {(options.groupby as string[])?.length > 0 ||
       hasAggregate(options.columns as AdvancedQueryColumn[]) ? (
-        <div>Having block goes here</div>
+        <JqueryQueryBuilder
+          fieldData={fieldData}
+          getJqueryBuilderInstance={getJqueryBuilderInstance}
+          icons={{
+            add_group: 'fa fa-plus-circle',
+            add_rule: 'fa fa-plus',
+            remove_group: 'fa fa-times realign',
+            remove_rule: 'fa fa-times realign',
+            error: 'fa fa-exclamation-triangle',
+          }}
+          rules={createQueryBuilderRules({}, options.filter)}
+        />
       ) : (
         <Alert variant="warning" className="mt-2">
-          Having clause requires or aggregate columns
+          Having clause requires groupBy or aggregate columns
         </Alert>
       )}
     </>
