0e3d1772da41ac3a70c5910bb738758fcea961e0
Thatcher Kaliisa <kaliithatmau@gmail.com>
2022-03-02 15:31:30 +0300

    Fix typing errors

--- frontend/src/components/JqueryQueryBuilder/JqueryQueryBuilder.tsx
+++ frontend/src/components/JqueryQueryBuilder/JqueryQueryBuilder.tsx
@@ -2,8 +2,12 @@ import * as jQueryQueryBuilder from 'jQuery-QueryBuilder';
 import 'jQuery-QueryBuilder/dist/css/query-builder.default.min.css';
 import React, { FunctionComponent, useEffect } from 'react';
 import {
+  AdvancedQueryHaving,
+  AdvancedQueryHavingComparator,
   JqueryQueryBuilderFieldData,
   JqueryQueryBuilderFilter,
+  JqueryQueryBuilderHaving,
+  JqueryQueryBuilderHavingComparator,
   JqueryQueryBuilderIcons,
 } from '../../types/operations';
 import { aggregateOptions } from '../AdvancedHavingQueryBuilder';
@@ -14,7 +18,7 @@ export interface JqueryQueryBuilderProps {
   fieldData: JqueryQueryBuilderFieldData[];
   getJqueryBuilderInstance: (jqInstance: any) => void;
   icons?: JqueryQueryBuilderIcons;
-  rules: JqueryQueryBuilderFilter;
+  rules: JqueryQueryBuilderFilter | JqueryQueryBuilderHaving;
 }
 
 const JqueryQueryBuilder: FunctionComponent<JqueryQueryBuilderProps> = ({
@@ -34,22 +38,33 @@ const JqueryQueryBuilder: FunctionComponent<JqueryQueryBuilderProps> = ({
     jq.init();
 
     if (Object.keys(rules).length > 0) {
-      if (Object.prototype.toString.call(rules?.rules[0].value) === '[object Object]') {
-        console.log(rules);
-        if ('plain' in rules.rules[0].value) {
-          rules.rules[0].value = rules.rules[0].value['plain'];
+      if (
+        rules &&
+        Object.prototype.toString.call(
+          ((rules as JqueryQueryBuilderHaving)['rules'] as JqueryQueryBuilderHavingComparator[])[0]
+            .value,
+        ) === '[object Object]'
+      ) {
+        const element = (
+          (rules as JqueryQueryBuilderHaving)['rules'] as JqueryQueryBuilderHavingComparator[]
+        )[0];
+        if (element.value && 'plain' in (element.value as { plain: string | number })) {
+          element.value = (element.value as { plain: string | number })['plain'];
         }
-        const previousValue = rules.rules[0].value;
-        rules.rules[0].value = rules.rules[0]['values'];
-        delete rules.rules[0].value;
-        rules.rules[0].values = sortAggregateOptions(aggregateOptions, previousValue.aggregate).map(
-          (option) => {
-            return {
-              value: `${previousValue.column},${option}`,
-              label: `${option}(${previousValue.column})`,
-            };
-          },
-        );
+        const previousValue: { column: string; aggregate: string } = element.value as {
+          column: string;
+          aggregate: string;
+        };
+        delete element.value;
+        element.values = sortAggregateOptions(
+          aggregateOptions,
+          (previousValue as { column: string; aggregate: string }).aggregate,
+        ).map((option) => {
+          return {
+            value: `${previousValue?.column},${option}`,
+            label: `${option}(${previousValue?.column})`,
+          };
+        });
       }
       jq.setRules(rules);
     }
