03ced09ea3973928dc0799572705987f2b44386e
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-05 21:11:57 +0300

    Show default select statement when a source is selected

--- frontend/src/components/SQLEditor/SQLEditor.tsx
+++ frontend/src/components/SQLEditor/SQLEditor.tsx
@@ -1,5 +1,5 @@
 import { PostgreSQL, sql } from '@codemirror/lang-sql';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { OperationMap } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
 import { CodeMirrorNext } from '../CodeMirrorNext';
@@ -11,9 +11,20 @@ interface ComponentProps {
 }
 
 const SQLEditor: FC<ComponentProps> = ({ source }) => {
-  console.log(source?.toJS());
+  const [value, setValue] = useState('');
 
-  return <CodeMirrorNext extensions={[sql({ dialect: PostgreSQL, upperCaseKeywords: true })]} />;
+  useEffect(() => {
+    if (source) {
+      setValue(`SELECT * FROM "${source.get('schema')}"."${source.get('active_mirror_name')}";`);
+    }
+  }, [source]);
+
+  return (
+    <CodeMirrorNext
+      value={value}
+      extensions={[sql({ dialect: PostgreSQL, upperCaseKeywords: true })]}
+    />
+  );
 };
 
 export { SQLEditor };
