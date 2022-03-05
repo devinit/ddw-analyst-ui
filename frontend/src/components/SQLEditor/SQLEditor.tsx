be3a1a4d5d69be404ca8525925b693fd1845319a
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-05 21:34:11 +0300

    Show configs for existing operations

--- frontend/src/components/SQLEditor/SQLEditor.tsx
+++ frontend/src/components/SQLEditor/SQLEditor.tsx
@@ -1,7 +1,9 @@
 import { PostgreSQL, sql } from '@codemirror/lang-sql';
 import React, { FC, useEffect, useState } from 'react';
+import { format } from 'sql-formatter';
 import { OperationMap } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
+import { getSourceIDFromOperation } from '../../utils';
 import { CodeMirrorNext } from '../CodeMirrorNext';
 
 interface ComponentProps {
@@ -10,14 +12,27 @@ interface ComponentProps {
   onUpdateOperation: (operation: OperationMap) => void;
 }
 
-const SQLEditor: FC<ComponentProps> = ({ source }) => {
+const SQLEditor: FC<ComponentProps> = ({ source, operation }) => {
   const [value, setValue] = useState('');
 
   useEffect(() => {
-    if (source) {
-      setValue(`SELECT * FROM "${source.get('schema')}"."${source.get('active_mirror_name')}";`);
+    if (source && !operation) {
+      setValue(
+        format(`SELECT * FROM "${source.get('schema')}"."${source.get('active_mirror_name')}";`),
+      );
     }
-  }, [source]);
+    if (operation && source) {
+      const operationSource = getSourceIDFromOperation(operation);
+      // check if the source has been changed
+      if (operationSource !== source.get('id')) {
+        setValue(
+          format(`SELECT * FROM "${source.get('schema')}"."${source.get('active_mirror_name')}";`),
+        );
+      } else {
+        setValue(format(operation.get('operation_query') as string));
+      }
+    }
+  }, [source, operation]);
 
   return (
     <CodeMirrorNext
