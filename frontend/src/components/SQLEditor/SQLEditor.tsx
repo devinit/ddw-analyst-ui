1b9a468d7fb61e7eec599acc8db1c78460137a67
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-05 20:54:49 +0300

    Add CodeMirrorNext to SQLEditor

--- frontend/src/components/SQLEditor/SQLEditor.tsx
+++ frontend/src/components/SQLEditor/SQLEditor.tsx
@@ -1,7 +1,9 @@
+import { PostgreSQL, sql } from '@codemirror/lang-sql';
 import React, { FC } from 'react';
+import { CodeMirrorNext } from '../CodeMirrorNext';
 
 const SQLEditor: FC = () => {
-  return <div>Editor Goes Here</div>;
+  return <CodeMirrorNext extensions={[sql({ dialect: PostgreSQL })]} />;
 };
 
 export { SQLEditor };
