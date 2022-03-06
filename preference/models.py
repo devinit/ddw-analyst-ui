eef0425d6a9812ed5db16ce0d22547dbd99fe9a1
Pauline <pauline@DI-UG000174.local>
2022-03-06 21:50:34 +0300

    Modified the preferences field with a default value

--- preference/models.py
+++ preference/models.py
@@ -3,10 +3,7 @@ from core.models import BaseEntity
 
 # Create your models here.
 class Preference(BaseEntity):
-    preferences = models.JSONField(blank=True, null=True, default=dict)
-    global_query = models.BooleanField(default=False)
+    preferences = models.JSONField(blank=True, null=True, default={"queryBuilder" : "advanced"})
+    global_choice = models.BooleanField(default=False)
 
-Preference(preferences={
-    "queryBuilder": "advanced"
-}
-)
+
