92f277e64b9ded3f829c007ab1a044d300b475f2
Pauline <pauline@DI-UG000174.local>
2022-03-07 11:12:27 +0300

    Added list_filter to filter prefences by global field

--- preference/admin.py
+++ preference/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Preference
 
 # Register your models here.
-admin.site.register(Preference)
+class PreferenceAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'global_choice')
+
+admin.site.register(Preference, PreferenceAdmin)
