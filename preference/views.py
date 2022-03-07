a1ce72ab9802a9b41492fc2cc666cdf1d7b3bbba
Pauline <pauline@DI-UG000174.local>
2022-03-07 21:50:33 +0300

    Modified permisions for the preference views

--- preference/views.py
+++ preference/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Preference
 from .serializers import PreferenceSerializer
 
 # Create your views here.
 class PreferedQueryBuilder(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Preference.objects.all()
     serializer_class = PreferenceSerializer
 
 class UpdatePreferedQueryBuilder(generics.UpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Preference.objects.all()
     serializer_class = PreferenceSerializer
