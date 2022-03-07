532a4b1128632af49f8779bdb973553e37f1ce02
Pauline <pauline@DI-UG000174.local>
2022-03-07 13:12:23 +0300

    Created a variable for the default value of the preferences field's JSONField

--- preference/models.py
+++ preference/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from core.models import BaseEntity
 
 # Create your models here.
+def default_value():
+    return {"queryBuilder" : "advanced"}
 class Preference(BaseEntity):
-    preferences = models.JSONField(blank=True, null=True, default={"queryBuilder" : "advanced"})
+    preferences = models.JSONField(blank=True, null=True, default=default_value)
     global_choice = models.BooleanField(default=False)
 
 
