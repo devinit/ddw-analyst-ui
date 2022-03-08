dd5f2d2efdb56b62865eb41f8abe43ac191a55c3
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-08 16:36:34 +0300

    Refactor default props & types

--- frontend/src/components/TreeView/utils/types.ts
+++ frontend/src/components/TreeView/utils/types.ts
@@ -3,23 +3,17 @@ import { ReactElement } from 'react';
 export interface TreeViewProps {
   data: Data[];
   depth?: number;
-
   deleteElement?: ReactElement;
-
   getStyleClass?: (node: Data) => void;
-
   isCheckable?: (node: Data, depth?: number) => void;
   isDeletable?: (node: Data, depth?: number) => void;
   isExpandable?: (node: Data, depth?: number) => void;
-
   loadingElement?: ReactElement;
   noChildrenAvailableMessage?: string;
-
   onCheckToggle?: (data: Data[], depth?: number) => void;
   onDelete?: (node: Data, data: Data[], depth?: number) => void;
   onExpandToggle?: (node: Data, depth?: number) => void;
   onUpdate?: (data: unknown[], depth?: number) => void;
-
   transitionEnterTimeout?: number;
   transitionExitTimeout?: number;
 }
