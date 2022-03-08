3213a8c10984ce749b4b27929fe5aa2cf70284a3
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-08 10:35:06 +0300

    Split view in prep for sidebar

--- frontend/src/components/SQLEditor/SQLEditor.tsx
+++ frontend/src/components/SQLEditor/SQLEditor.tsx
@@ -2,7 +2,7 @@ import { PostgreSQL, sql } from '@codemirror/lang-sql';
 import { Completion } from '@codemirror/autocomplete';
 import { fromJS } from 'immutable';
 import React, { FC, useEffect, useState } from 'react';
-import { Alert, Button } from 'react-bootstrap';
+import { Alert, Button, Col, Row } from 'react-bootstrap';
 import { format } from 'sql-formatter';
 import { Operation, OperationData, OperationDataList, OperationMap } from '../../types/operations';
 import { Column, ColumnList, SourceMap } from '../../types/sources';
@@ -103,35 +103,38 @@ const SQLEditor: FC<ComponentProps> = ({ source, operation, onUpdateOperation })
   if (!source) return null;
 
   return (
-    <>
-      <Alert variant="warning" show={!!error}>
-        {error}
-      </Alert>
-      <CodeMirrorNext
-        value={value}
-        extensions={[
-          sql({
-            dialect: PostgreSQL,
-            upperCaseKeywords: true,
-            schema: getSchema(`${source.get('active_mirror_name')}`, source),
-          }),
-        ]}
-        onChange={onChange}
-      />
-      <div className="mt-2">
-        <Button variant="dark" onClick={onRunQuery} size="sm">
-          <i className="fa fa-play mr-1" /> Run
-        </Button>
-      </div>
-      <OperationPreview
-        className="mt-2"
-        show
-        data={fromJS(data) as OperationDataList}
-        onClose={() => true}
-        tableOnly
-        loading={dataLoading}
-      />
-    </>
+    <Row>
+      <Col lg={9}>
+        <Alert variant="warning" show={!!error}>
+          {error}
+        </Alert>
+        <CodeMirrorNext
+          value={value}
+          extensions={[
+            sql({
+              dialect: PostgreSQL,
+              upperCaseKeywords: true,
+              schema: getSchema(`${source.get('active_mirror_name')}`, source),
+            }),
+          ]}
+          onChange={onChange}
+        />
+        <div className="mt-2">
+          <Button variant="dark" onClick={onRunQuery} size="sm">
+            <i className="fa fa-play mr-1" /> Run
+          </Button>
+        </div>
+        <OperationPreview
+          className="mt-2"
+          show
+          data={fromJS(data) as OperationDataList}
+          onClose={() => true}
+          tableOnly
+          loading={dataLoading}
+        />
+      </Col>
+      <Col lg={3}>Sidebar Goes Here</Col>
+    </Row>
   );
 };
 
