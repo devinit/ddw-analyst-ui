4e8ebb43b04e69687f4b695b4a70caa33446c73c
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-08 11:30:09 +0300

    Add basic styling to SourcesMetaData

--- frontend/src/components/SourcesMetaData/SourcesMetaData.tsx
+++ frontend/src/components/SourcesMetaData/SourcesMetaData.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  height: 100%;
+`;
 
 const SourcesMetaData: FC = () => {
-  return <div>Sources Go Here</div>;
+  return <StyledWrapper className="border-left p-2">Sources Go Here</StyledWrapper>;
 };
 
 export { SourcesMetaData };
