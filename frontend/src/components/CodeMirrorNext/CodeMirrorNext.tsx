dba2b6e8b1864d2987055c0e70e22eb4d24cbe4f
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-09 08:48:42 +0300

    Give CodeMirrorNext a default height

--- frontend/src/components/CodeMirrorNext/CodeMirrorNext.tsx
+++ frontend/src/components/CodeMirrorNext/CodeMirrorNext.tsx
@@ -1,11 +1,16 @@
 import { EditorView, ReactCodeMirrorProps, useCodeMirror } from '@uiw/react-codemirror';
 import React, { FC, useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
 interface CustomProps {
   readOnly?: boolean;
 }
 
 type ComponentProps = ReactCodeMirrorProps & CustomProps;
+
+const StyledDiv = styled.div<ComponentProps>`
+  height: ${(props) => props.height};
+`;
 
 const CodeMirrorNext: FC<ComponentProps> = ({ readOnly, ...props }) => {
   const editor = useRef<HTMLDivElement>(null);
@@ -25,7 +30,9 @@ const CodeMirrorNext: FC<ComponentProps> = ({ readOnly, ...props }) => {
     }
   }, [editor.current]);
 
-  return <div ref={editor} contentEditable={false} />;
+  return <StyledDiv ref={editor} contentEditable={false} />;
 };
+
+CodeMirrorNext.defaultProps = { height: '250px' };
 
 export { CodeMirrorNext };
