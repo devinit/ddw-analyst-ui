0a2e25a5f236a57f8ee9a47568ec928ba8e1945b
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-09 16:38:26 +0300

    Add custom and/or selector

--- frontend/src/components/ReactQueryBuilder/ReactQueryBuilder.tsx
+++ frontend/src/components/ReactQueryBuilder/ReactQueryBuilder.tsx
@@ -4,8 +4,9 @@ import {
 } from '@react-querybuilder/bootstrap';
 import React, { FC } from 'react';
 import { Button } from 'react-bootstrap';
-import QueryBuilder, { QueryBuilderProps } from 'react-querybuilder';
+import QueryBuilder, { NameLabelPair, QueryBuilderProps } from 'react-querybuilder';
 import 'react-querybuilder/dist/query-builder.scss';
+import { Dropdown, DropdownProps } from 'semantic-ui-react';
 import DeleteAction from './DeleteAction';
 import './styles.css';
 import { getClasses } from './utils/config';
@@ -15,6 +16,29 @@ const ReactQueryBuilder: FC<QueryBuilderProps> = (props) => {
     <QueryBuilder
       controlElements={{
         ...bootstrapControlElements,
+        combinatorSelector: (props) => {
+          const onChange = (
+            _event: React.SyntheticEvent<HTMLElement, Event>,
+            data: DropdownProps,
+          ) => {
+            props.handleOnChange(data.value);
+          };
+
+          return (
+            <Dropdown
+              className="col-1"
+              fluid
+              selection
+              options={props.options.map((option) => ({
+                key: (option as NameLabelPair).name,
+                value: (option as NameLabelPair).name,
+                text: option.label,
+              }))}
+              onChange={onChange}
+              value={props.value}
+            />
+          );
+        },
         addRuleAction: (props) => (
           <Button variant="danger" size="sm" onClick={props.handleOnClick}>
             Add Rule
