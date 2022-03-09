734db0b67db1b85c95cfb13e527f5ab359e36554
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-09 20:33:44 +0300

    Update getClasses to bare minimum

--- frontend/src/components/ReactQueryBuilder/utils/config.ts
+++ frontend/src/components/ReactQueryBuilder/utils/config.ts
@@ -1,15 +1,8 @@
-const fieldClasses = 'form-control col-3 border-top border-left border-right rounded';
-const operatorClasses =
-  'form-control col-2 text-center border-top border-left border-right rounded';
-const combinatorClasses =
-  'form-control col-1 text-center border-top border-left border-right rounded';
+import { Classnames } from 'react-querybuilder';
 
-export const getClasses = (defaultClasses: Record<string, unknown>): Record<string, unknown> => {
+export const getClasses = (defaultClasses: Partial<Classnames>): Partial<Classnames> => {
   return {
     ...defaultClasses,
-    fields: `${defaultClasses.fields} ${fieldClasses}`,
-    operators: `${defaultClasses.operators} ${operatorClasses}`,
     value: `${defaultClasses.value} col-3`,
-    combinators: `${defaultClasses.combinators} ${combinatorClasses}`,
   };
 };
