78fe9687f5aa7d4ada863c59c86c769f27b4c783
Pauline <pauline@DI-UG000174.local>
2022-03-10 11:53:24 +0300

    Changed the class name of the prefences views from PreferedQueryBuilder to PreferedChoice

--- preference/views.py
+++ preference/views.py
@@ -1,14 +1,19 @@
 from rest_framework import generics, permissions
+from knox.auth import TokenAuthentication
 from .models import Preference
 from .serializers import PreferenceSerializer
+from core.permissions import IsOwnerOrReadOnly
+
 
 # Create your views here.
-class PreferedQueryBuilder(generics.ListAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class PreferedChoice(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (permissions.AllowAny,)
     queryset = Preference.objects.all()
     serializer_class = PreferenceSerializer
 
-class UpdatePreferedQueryBuilder(generics.UpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class UpdatePreferedChoice(generics.UpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (permissions.AllowAny,)
     queryset = Preference.objects.all()
     serializer_class = PreferenceSerializer
