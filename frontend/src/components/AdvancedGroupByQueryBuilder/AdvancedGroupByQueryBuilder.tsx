ced3c5ad071336686fa5d6da961c773be53ae1c7
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-03-14 09:44:48 +0300

    Required explicitly selected columns

--- frontend/src/components/AdvancedGroupByQueryBuilder/AdvancedGroupByQueryBuilder.tsx
+++ frontend/src/components/AdvancedGroupByQueryBuilder/AdvancedGroupByQueryBuilder.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
+import { Alert } from 'react-bootstrap';
 import { AdvancedQueryColumn } from '../../types/operations';
 import { SourceMap } from '../../types/sources';
 import { ColumnSelector } from '../AdvancedSelectQueryBuilder/ColumnSelector';
-import { AdvancedQueryContext } from '../QuerySentenceBuilder';
+import { AdvancedQueryContext, QueryContextProps } from '../QuerySentenceBuilder';
 
 interface ComponentProps {
   source: SourceMap;
@@ -10,24 +11,32 @@ interface ComponentProps {
   onUpdateConfig?: (config: { columns: AdvancedQueryColumn[] }) => void;
 }
 
-const AdvancedGroupByQueryBuilder: FunctionComponent<ComponentProps> = ({ source }) => (
-  <div className="mb-3">
-    <AdvancedQueryContext.Consumer>
-      {({ options }) => (
-        <ColumnSelector
-          show
-          usage="groupby"
-          nameOnly
-          source={source}
-          columns={
-            options.groupby
-              ? options.groupby.map<AdvancedQueryColumn>((column) => ({ name: column }))
-              : []
-          }
-        />
-      )}
-    </AdvancedQueryContext.Consumer>
-  </div>
-);
+const AdvancedGroupByQueryBuilder: FunctionComponent<ComponentProps> = ({ source }) => {
+  const { options } = useContext<QueryContextProps>(AdvancedQueryContext);
+
+  if (options.selectall) {
+    return <Alert variant="warning">Group By requires you to explicitly select columns</Alert>;
+  }
+
+  return (
+    <div className="mb-3">
+      <AdvancedQueryContext.Consumer>
+        {({ options }) => (
+          <ColumnSelector
+            show
+            usage="groupby"
+            nameOnly
+            source={source}
+            columns={
+              options.groupby
+                ? options.groupby.map<AdvancedQueryColumn>((column) => ({ name: column }))
+                : []
+            }
+          />
+        )}
+      </AdvancedQueryContext.Consumer>
+    </div>
+  );
+};
 
 export { AdvancedGroupByQueryBuilder };
