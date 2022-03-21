827f97ba386897c2d085e5fc9bb1b6256b67d583
Thatcher Kaliisa <kaliithatmau@gmail.com>
2022-03-21 16:15:43 +0300

    Cater for aggregate having columns in the edit screen

--- frontend/src/components/AdvancedHavingQueryBuilder/utils/index.ts
+++ frontend/src/components/AdvancedHavingQueryBuilder/utils/index.ts
@@ -1,15 +1,10 @@
-import { RuleGroupType, generateID } from 'react-querybuilder';
+import { generateID } from 'react-querybuilder';
 import {
   AdvancedQueryColumn,
   AdvancedQueryHaving,
   AdvancedQueryOptions,
 } from '../../../types/operations';
 import { Column, ColumnList } from '../../../types/sources';
-import { convertJqOperatorToDDW } from '../../JqueryQueryBuilder/utils';
-import {
-  JqueryQueryBuilderHaving,
-  JqueryQueryBuilderHavingComparator,
-} from '../../JqueryQueryBuilder/utils/types';
 
 export const sortAggregateOptions = (options: string[], selected: string): string[] => {
   if (options.indexOf(selected) === 0) {
@@ -28,28 +23,6 @@ export const getColumnFromAlias = (
   columns: AdvancedQueryColumn[],
 ): AdvancedQueryColumn => columns.find((col) => col.alias === alias) as AdvancedQueryColumn;
 
-export const getHavingQueryValues = (query: JqueryQueryBuilderHaving): JqueryQueryBuilderHaving => {
-  query.rules?.map((rule, index: number) => {
-    if (rule.hasOwnProperty('condition')) {
-      getHavingQueryValues(rule as JqueryQueryBuilderHaving);
-    } else {
-      const element = (
-        (query as JqueryQueryBuilderHaving)['rules'] as JqueryQueryBuilderHavingComparator[]
-      )[index];
-      if ('plain' in (element.value as { plain: string | number })) {
-        element.value = (element.value as { plain: string | number })['plain'];
-      } else {
-        const activeValue: { column: string; aggregate: string } = element.value as {
-          column: string;
-          aggregate: string;
-        };
-        element.value = `${activeValue?.column},${activeValue.aggregate}`;
-      }
-    }
-  });
-
-  return query;
-};
 export const getGroupByColumns = ({
   columns = [],
   groupby: columnNames,
@@ -133,73 +106,6 @@ export const parseHavingQueryReact = (
   return finalElement;
 };
 
-export const parseHavingQuery = (
-  finalElement: any,
-  condition: string,
-  rulesObject: any,
-  aggregateColumns?: AdvancedQueryColumn[],
-  columns?: AdvancedQueryColumn[],
-): AdvancedQueryHaving => {
-  if (rulesObject.hasOwnProperty('condition')) {
-    finalElement[`$${rulesObject.condition.toLowerCase()}`] = [];
-    finalElement = parseHavingQuery(
-      finalElement,
-      rulesObject.condition,
-      rulesObject.rules,
-      aggregateColumns,
-      columns,
-    );
-  } else {
-    for (let index = 0; index < rulesObject.length; index++) {
-      if (Array.isArray(rulesObject) && rulesObject[index].condition) {
-        finalElement[`$${condition.toLowerCase()}`].push(
-          parseHavingQuery(
-            {},
-            rulesObject[index].condition,
-            rulesObject[index].rules,
-            aggregateColumns,
-            columns,
-          ),
-        );
-      } else {
-        if (!finalElement.hasOwnProperty(`$${condition.toLowerCase()}`)) {
-          finalElement[`$${condition.toLowerCase()}`] = [];
-        }
-        if ((aggregateColumns as AdvancedQueryColumn[]).length > 0) {
-          aggregateColumns?.map((column) => {
-            if (column.name === rulesObject[index].field) {
-              finalElement[`$${condition.toLowerCase()}`].push({
-                column: rulesObject[index].field,
-                comp: convertJqOperatorToDDW(rulesObject[index].operator),
-                aggregate: column.aggregate,
-                value: { plain: rulesObject[index].value },
-              });
-            }
-          });
-        } else {
-          if (typeof rulesObject[index].value === 'number') {
-            finalElement[`$${condition.toLowerCase()}`].push({
-              column: getColumnFromAlias(rulesObject[index].field, columns as AdvancedQueryColumn[])
-                .name,
-              comp: convertJqOperatorToDDW(rulesObject[index].operator),
-              value: { plain: rulesObject[index].value },
-            });
-          } else {
-            const receivedString = rulesObject[index].value.split(',');
-            finalElement[`$${condition.toLowerCase()}`].push({
-              column: rulesObject[index].field,
-              comp: convertJqOperatorToDDW(rulesObject[index].operator),
-              value: { column: receivedString[0], aggregate: receivedString[1] },
-            });
-          }
-        }
-      }
-    }
-  }
-
-  return finalElement;
-};
-
 export const createQueryBuilderRules = (finalElement: any, query: any) => {
   if (query && query.hasOwnProperty('$or')) {
     finalElement['combinator'] = 'or';
@@ -220,13 +126,21 @@ export const createQueryBuilderRules = (finalElement: any, query: any) => {
           if (!finalElement.hasOwnProperty('rules')) {
             finalElement['rules'] = [];
           } else {
-            console.log(query[index]);
-            finalElement['rules'].push({
-              id: generateID(),
-              field: query[index].column,
-              operator: query[index].comp,
-              value: `${query[index].value.column},${query[index].value.aggregate}`,
-            });
+            if ('aggregate' in query[index]) {
+              finalElement['rules'].push({
+                id: generateID(),
+                field: query[index].column,
+                operator: query[index].comp,
+                value: query[index].value['plain'],
+              });
+            } else {
+              finalElement['rules'].push({
+                id: generateID(),
+                field: query[index].column,
+                operator: query[index].comp,
+                value: `${query[index].value.column},${query[index].value.aggregate}`,
+              });
+            }
           }
         }
       }
@@ -235,21 +149,3 @@ export const createQueryBuilderRules = (finalElement: any, query: any) => {
 
   return finalElement;
 };
-
-// query.rules?.map((rule, index: number) => {
-//   if (rule.hasOwnProperty('condition')) {
-//     getHavingQueryValues(rule as JqueryQueryBuilderHaving);
-//   } else {
-//     const element = (
-//       (query as JqueryQueryBuilderHaving)['rules'] as JqueryQueryBuilderHavingComparator[]
-//     )[index];
-//     if ('plain' in (element.value as { plain: string | number })) {
-//       element.value = (element.value as { plain: string | number })['plain'];
-//     } else {
-//       const activeValue: { column: string; aggregate: string } = element.value as {
-//         column: string;
-//         aggregate: string;
-//       };
-//       element.value = `${activeValue?.column},${activeValue.aggregate}`;
-//     }
-//   }
