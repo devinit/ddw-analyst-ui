9212952a6f73dbfe28e69f375058f27950b04e72
Pauline <pauline@DI-UG000174.local>
2022-03-29 16:29:36 +0300

    Prefered querybuilder from localstorage showsup on clicking QueryBuilder otherwise modal shows up

--- frontend/src/pages/QueryBuilderChooser/QueryBuilderChooser.tsx
+++ frontend/src/pages/QueryBuilderChooser/QueryBuilderChooser.tsx
@@ -70,6 +70,7 @@ const QueryBuilderChooser: FC<RouteComponentProps> = () => {
           }
         })
         .catch((err) => console.log(err));
+      console.log(token);
     });
     if (checked === true) {
       localForage.setItem(localForageKeys.PREFERENCES, selectedOption);
