82c0db7bcd8b45298c272971cf8bed71595af631
Wakibi Chris <wakibic@gmail.com>
2022-05-16 14:29:30 +0300

    Fix failing tests

--- frontend/src/actions/__tests__/sources.spec.ts
+++ frontend/src/actions/__tests__/sources.spec.ts
@@ -4,7 +4,7 @@ import { fetchSources } from '../sources';
 test('should create an action to fetch sources', () => {
   const expectedAction = {
     type: FETCH_SOURCES,
-    payload: { limit: 10, offset: 0, link: '', search: '' },
+    payload: { limit: 10, offset: 0, link: '', search: '', frozen: 0 },
   };
 
   expect(fetchSources()).toEqual(expectedAction);
