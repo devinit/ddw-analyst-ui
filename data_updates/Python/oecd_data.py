0f7a6cb4e2b80360704a74758805b2cf7ee96db3
Thatcher Kaliisa <kaliithatmau@gmail.com>
2022-05-30 12:24:17 +0300

    Fix import syntax error

--- data_updates/Python/oecd_data.py
+++ data_updates/Python/oecd_data.py
@@ -5,7 +5,7 @@ import glob
 import numpy
 from numpy import NaN
 import pandas as pd
-from utils.data_updates import push_folder_to_github
+from data_updates.utils import push_folder_to_github
 
 
 current_date = datetime.datetime.now()
