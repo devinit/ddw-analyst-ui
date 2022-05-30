4150359ade964423072d341013a601472f830bfd
Thatcher Kaliisa <kaliithatmau@gmail.com>
2022-05-30 09:40:37 +0300

    Add tasks to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
+const webpackConfig = require('./webpack.config.js');
+const { merge } = require('webpack-merge');
+
+const webpackConfigProduction = merge(webpackConfig, {
+  devtool: false,
+  mode: 'production',
+  watch: false,
+});
+
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    // Tasks and subtasks are defined from here on
+    clean: {
+      contents: ['frontend/static/frontend/css/*', 'frontend/static/frontend/js/*'],
+    },
+    webpack: {
+      prod: webpackConfigProduction,
+      dev: webpackConfig,
+    },
   });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-webpack');
+
+  grunt.task.registerTask('default', 'Run the default task', ['clean', 'webpack:dev']);
+  grunt.task.registerTask('build', 'Run production build', ['clean', 'webpack:prod']);
 };
