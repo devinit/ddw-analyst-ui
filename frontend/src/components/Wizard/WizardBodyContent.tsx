aced8df2903b674d49c245bfe499f4f712c11b3e
Thatcher Kaliisa <kaliithatmau@gmail.com>
2022-06-08 13:36:30 +0300

    Add children type to WizardBodyContent component

--- frontend/src/components/Wizard/WizardBodyContent.tsx
+++ frontend/src/components/Wizard/WizardBodyContent.tsx
@@ -1,7 +1,10 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { Tab, TabPaneProps } from 'react-bootstrap';
 
-const WizardBodyContent: FunctionComponent<TabPaneProps> = ({ children, ...props }) => {
+interface ComponentProps extends TabPaneProps {
+  children?: React.ReactNode;
+}
+const WizardBodyContent: FunctionComponent<ComponentProps> = ({ children, ...props }) => {
   const [viewed, setViewed] = useState(props.active);
   useEffect(() => {
     if (props.active) {
