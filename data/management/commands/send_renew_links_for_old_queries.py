c3e68c7f1412c87d9ee6e68307e9092dd0769fd0
Wakibi Chris <wakibic@gmail.com>
2022-06-10 14:32:25 +0300

    Process frozen data and sources

--- data/management/commands/send_renew_links_for_old_queries.py
+++ data/management/commands/send_renew_links_for_old_queries.py
@@ -19,42 +19,51 @@ ARCHIVE_SUBJECT = 'Frozen data renewal'
 DAYS_TO_SEND_TO_PM = 7
 DAYS_TO_DELETE = 14
 FROZEN_DATASET_SCHEMA = 'dataset'
+DATA_TYPES = {
+    'operation': Operation,
+    'frozen_table': FrozenData,
+    'frozen_dataset': SavedQueryData,
+}
 
 
 class Command(BaseCommand):
     help = 'Gets old queries and sends reminders to owners'
 
     def handle(self, *args, **options):
-        self.process_operations(False, QUERY_SUBJECT, RENEWAL_MESSAGE)
-        self.process_operations(True, QUERY_SUBJECT, RENEWAL_MESSAGE)
+        self.process_renewal_links(False, QUERY_SUBJECT, RENEWAL_MESSAGE)
+        self.process_renewal_links(True, QUERY_SUBJECT, RENEWAL_MESSAGE)
+        self.delete_old_operations()
+        self.delete_old_frozen_sources()
+        self.delete_old_frozen_datasets()
 
     @transaction.atomic
-    def process_operations(renewal_sent=False, subject='', message=''):
+    def process_renewal_links(renewal_sent=False, subject='', message=''):
         reset_generator = QueryResetTokenGenerator()
         time_difference = SIX_MONTHS
         # This will handle sending to Dean after expiry of 7 days since last one was sent to owner
         if renewal_sent:
             time_difference += DAYS_TO_SEND_TO_PM
-        old_operations = Operation.objects.filter(
-            last_accessed__lte=datetime.now()-timedelta(days=time_difference)
-        ).filter(renewal_sent=renewal_sent).select_related('user')
-        processed_operations = []
-        for old_operation in old_operations:
-            reset_token = reset_generator.make_token(old_operation)
-            reset_link = reverse('renewal_view', kwargs={'id': old_operation.id, 'token': reset_token})
-            reset_link = f'{settings.BASE_URL}{reset_link}'
-            recipients = [user[1] for user in settings.ADMINS]
-            if old_operation.user and old_operation.user.username:
-                recipients.append(old_operation.user.username)
-            subject = f'{subject} ({old_operation})'
-            message = f'{message} {reset_link}'
-            try:
-                send_emails(subject, message, recipients)
-                old_operation.renewal_sent = True
-                processed_operations.append(old_operation)
-            except:
-                pass
-            Operation.objects.bulk_update(processed_operations, ['renewal_sent'], batch_size=1000)
+        for name, model in DATA_TYPES.items():
+            old_objects = model.objects.filter(
+                last_accessed__lte=datetime.now()-timedelta(days=time_difference)
+            ).filter(renewal_sent=renewal_sent).select_related('user')
+            processed_objects = []
+            for old_object in old_objects:
+                reset_token = reset_generator.make_token(old_object)
+                reset_link = reverse('renewal_view', kwargs={'id': old_object.id, 'token': reset_token})
+                reset_link = f'{settings.BASE_URL}{reset_link}'
+                recipients = [user[1] for user in settings.ADMINS]
+                if old_object.user and old_object.user.username:
+                    recipients.append(old_object.user.username)
+                subject = f'{subject} ({old_object})'
+                message = f'{message} {reset_link}'
+                try:
+                    send_emails(subject, message, recipients)
+                    old_object.renewal_sent = True
+                    processed_objects.append(old_object)
+                except:
+                    pass
+                model.objects.bulk_update(processed_objects, ['renewal_sent'], batch_size=1000)
 
     @transaction.atomic
     def delete_old_operations():
@@ -72,10 +81,6 @@ class Command(BaseCommand):
                 pass
 
     @transaction.atomic
-    def process_frozen_tables():
-        pass
-
-    @transaction.atomic
     def delete_old_frozen_sources():
         # Delete from sources table, delete table itself
         time_difference = SIX_MONTHS + DAYS_TO_DELETE
@@ -87,10 +92,6 @@ class Command(BaseCommand):
                 delete_archive(frozen_table.id)
             except:
                 pass
-
-    @transaction.atomic
-    def process_frozen_datasets():
-        pass
 
     @transaction.atomic
     def delete_old_frozen_datasets():
