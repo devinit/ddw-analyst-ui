b3d86437155163c76af6acb38ded5fa45bc3b10f
akmiller01 <alex.k.miller@gmail.com>
2022-12-20 10:30:38 -0500

    Reduce duplication on check own menu func

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -97,6 +97,26 @@ Cypress.Commands.add('getAccessToken', () => {
   return token;
 });
 
+Cypress.Commands.add('checkRenderOwnMenu', () => {
+  cy.visit('/queries/build');
+  cy.get('[id=help-nav-dropdown]').click();
+  cy.get('.dropdown-menu.show .nav-link')
+    .should('have.length.greaterThan', 0)
+    .then((links) => {
+      Array.prototype.forEach.call(links, (link, index) => {
+        if (index === links.length - 1) {
+          expect(link.href).to.equal('https://github.com/devinit/ddw-analyst-ui/issues/new');
+          expect(link.innerHTML).to.equal('Report Issue');
+        } else {
+          if (index === 0) {
+            expect(link.innerHTML).to.equal('About Page');
+          }
+          expect(link.href).to.contain('docs.google');
+        }
+      });
+    });
+});
+
 //
 //
 // -- This is a child command --
