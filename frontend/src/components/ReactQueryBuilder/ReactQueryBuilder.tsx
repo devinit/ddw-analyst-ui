ebf39fb6409994549f28b8cd89e8091ec6d46817
Thatcher Kaliisa <kaliithatmau@gmail.com>
2023-02-15 10:48:01 +0300

    Put CustomValueEditor in own file

--- frontend/src/components/ReactQueryBuilder/ReactQueryBuilder.tsx
+++ frontend/src/components/ReactQueryBuilder/ReactQueryBuilder.tsx
@@ -2,28 +2,15 @@ import {
   bootstrapControlClassnames,
   bootstrapControlElements,
 } from '@react-querybuilder/bootstrap';
-import React, { FC, useState } from 'react';
-import { Button, FormControl } from 'react-bootstrap';
-import QueryBuilder, { QueryBuilderProps, ValueEditorProps } from 'react-querybuilder';
+import React, { FC } from 'react';
+import { Button } from 'react-bootstrap';
+import QueryBuilder, { QueryBuilderProps } from 'react-querybuilder';
 import 'react-querybuilder/dist/query-builder.scss';
 import CustomSelector from './CustomSelector';
 import DeleteAction from './DeleteAction';
 import './styles.css';
 import { getClasses } from './utils/config';
-
-const CustomValueEditor = (props: ValueEditorProps) => {
-  const [value, setValue] = useState(props.value);
-
-  return (
-    <FormControl
-      value={value}
-      onChange={(event) => {
-        setValue(event.currentTarget.value);
-        props.handleOnChange(event.currentTarget.value);
-      }}
-    />
-  );
-};
+import { CustomValueEditor } from './CustomValueEditor';
 
 const ReactQueryBuilder: FC<QueryBuilderProps> = (props) => {
   return (
