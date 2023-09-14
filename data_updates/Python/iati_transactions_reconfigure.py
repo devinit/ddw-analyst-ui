ab84366ca0f5f167fe07749f91c3a1bf07efb75c
akmiller01 <alex.k.miller@gmail.com>
2023-09-14 14:20:51 -0400

    Get IDs from database

--- data_updates/Python/iati_transactions_reconfigure.py
+++ data_updates/Python/iati_transactions_reconfigure.py
@@ -7,6 +7,7 @@ from lxml import etree
 from lxml.etree import XMLParser
 from iati_transaction_spec import IatiFlat, A_DTYPES, A_NUMERIC_DTYPES, T_DTYPES, T_NUMERIC_DTYPES
 import boto3
+from botocore.exceptions import ClientError
 from datetime import datetime
 from sql_utils import batch, dataframe_records_gen
 
@@ -37,6 +38,8 @@ DATA_TABLENAME = "iati_transactions2"
 ACTIVITY_DATA_TABLENAME = "iati_activities"
 IATI_BUCKET_NAME = "di-s3"
 IATI_FOLDER_NAME = "iati_registry/"
+METADATA_SCHEMA = "repo"
+METADATA_TABLENAME = "iati_registry_metadata"
 
 
 def main():
@@ -63,16 +66,22 @@ def main():
         conn.execute(truncate_command)
         conn.execute(truncate_act_command)
 
-    paginator = s3_client.get_paginator('list_objects_v2')
-    page_iterator = paginator.paginate(Bucket=IATI_BUCKET_NAME, Prefix=IATI_FOLDER_NAME)
-    new_datasets = list()
-    for page in page_iterator:
-        new_datasets += [dataset['Key'][len(IATI_FOLDER_NAME):] for dataset in page['Contents']]
+    try:
+        datasets = Table(METADATA_TABLENAME, meta, schema=METADATA_SCHEMA, autoload_with=engine)
+    except sqlalchemy.exc.NoSuchTableError:
+        raise ValueError("No database found. Try running `iati_refresh.py` first.")
+
+    with engine.begin() as conn:
+        new_datasets = conn.execute(datasets.select().where(datasets.c.error == False)).fetchall()
+    new_dataset_ids = [dataset.id for dataset in new_datasets]
 
     bar = progressbar.ProgressBar()
-    for dataset_id in bar(new_datasets):
-        s3_object = s3_client.get_object(Bucket=IATI_BUCKET_NAME, Key=IATI_FOLDER_NAME+dataset_id)
-        download_xml = s3_object['Body'].read()
+    for dataset_id in bar(new_dataset_ids):
+        try:
+            s3_object = s3_client.get_object(Bucket=IATI_BUCKET_NAME, Key=IATI_FOLDER_NAME+dataset_id)
+            download_xml = s3_object['Body'].read()
+        except ClientError:
+            continue
 
         try:
             root = etree.fromstring(download_xml, parser=large_parser)
@@ -110,11 +119,12 @@ def main():
             flat_transaction_data[numeric_column] = pd.to_numeric(flat_transaction_data[numeric_column], errors='coerce')
         flat_transaction_data = flat_transaction_data.astype(dtype=T_DTYPES)
         flat_transaction_data_records = dataframe_records_gen(flat_transaction_data)
-        for flat_transaction_data_record in flat_transaction_data_records:
+        flat_transaction_data_record_batches = batch(flat_transaction_data_records, 50)
+        for flat_transaction_data_record_batch in flat_transaction_data_record_batches:
             with engine.begin() as conn:
                 conn.execute(
                     insert(transaction_table).values(
-                        [flat_transaction_data_record,]
+                        flat_transaction_data_record_batch
                     )
                 )
 
